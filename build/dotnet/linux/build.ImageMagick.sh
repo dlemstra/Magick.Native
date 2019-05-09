187a67ea26cc3b7f3a6cba576874ee9f07695c53
Dirk Lemstra <dirk@lemstra.org>
2019-05-09 17:28:01 +0200

    Added support for: libxml2, libturbojpeg, libtiff, libwebp, libopenjp2, liblcms2 and libraw_r.

--- build/dotnet/linux/build.ImageMagick.sh
+++ build/dotnet/linux/build.ImageMagick.sh
@@ -4,7 +4,12 @@ set -e
 export FLAGS="-O3 -fPIC"
 export STRICT_FLAGS="${FLAGS} -Wall"
 export CONFIGURE="./configure"
+export CMAKE_CONFIGURE="cmake"
 export MAKE="make"
+export SIMD_FLAG="-DWITH_SIMD=1"
+export CPPFLAGS="-I/usr/local/include -I/usr/local/include/openjpeg-2.3 -I/usr/local/include/libxml2 -I/usr/local/include/libraw"
+export LDFLAGS="-L/usr/local/lib"
+export CONDITIONAL_DISABLE_SHARED=""
 
 cd /src/ImageMagick/ImageMagick
 
@@ -14,12 +19,57 @@ chmod +x ./configure
 $CONFIGURE --static
 $MAKE install CFLAGS="$FLAGS"
 
+# Build libxml
+cd ../libxml
+autoreconf -fiv
+$CONFIGURE --with-python=no --enable-static --disable-shared CFLAGS="$FLAGS"
+$MAKE install
+
 # Build libpng
 cd ../png
 autoreconf -fiv
-$CONFIGURE --disable-mips-msa --disable-arm-neon --disable-powerpc-vsx --disable-shared
-$MAKE install CFLAGS="$FLAGS"
+$CONFIGURE --disable-mips-msa --disable-arm-neon --disable-powerpc-vsx --disable-shared CFLAGS="$FLAGS"
+$MAKE install
 
+# Build libjpeg-turbo
+cd ../jpeg
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_SHARED=off ${SIMD_FLAG} -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
+$MAKE install
+
+# Build libtiff
+cd ../tiff
+autoreconf -fiv
+$CONFIGURE ${CONDITIONAL_DISABLE_SHARED} CFLAGS="$FLAGS"
+$MAKE install
+
+# Build libwebpmux/demux
+cd ../webp
+autoreconf -fiv
+$CONFIGURE --enable-libwebpmux --enable-libwebpdemux --disable-shared CFLAGS="${FLAGS}"
+$MAKE install
+
+# Build openjpeg
+cd ../openjpeg
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS" || true
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
+$MAKE install
+cp bin/libopenjp2.a /usr/local/lib
+
+# Build lcms
+cd ../lcms
+autoreconf -fiv
+$CONFIGURE --disable-shared --prefix=/usr/local CFLAGS="$FLAGS"
+$MAKE install
+
+# Build libraw
+cd ../libraw
+chmod +x ./version.sh
+chmod +x ./shlib-version.sh
+autoreconf -fiv
+$CONFIGURE --disable-shared --disable-examples --disable-openmp --disable-jpeg --disable-jasper --prefix=/usr/local  CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
+$MAKE install
+
+# Build ImageMagick
 cd ../ImageMagick
-$CONFIGURE --disable-shared --disable-openmp --enable-static --enable-delegate-build --without-threads --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-quantum-depth=8 --disable-hdri PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
-$MAKE install CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS"
+$CONFIGURE --disable-shared --disable-openmp --enable-static --enable-delegate-build --without-threads --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-quantum-depth=8 --disable-hdri  CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS" PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
+$MAKE install
--- build/wasm/linux/build.ImageMagick.sh
+++ build/wasm/linux/build.ImageMagick.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib"
 export FLAGS="-O3"
 export STRICT_FLAGS="${FLAGS} -Wall"
 export CONFIGURE="emconfigure ./configure"
+export CMAKE_CONFIGURE="emconfigure cmake"
 export MAKE="emcmake make"
+export SIMD_FLAG="-DWITH_SIMD=0"
+export CPPFLAGS="-I/usr/local/include -I/usr/local/include/openjpeg-2.3 -I/usr/local/include/libxml2 -I/usr/local/include/libraw"
+export LDFLAGS="-L/usr/local/lib"
+export CONDITIONAL_DISABLE_SHARED="--disable-shared"
 
 cd /src/ImageMagick/ImageMagick
 
@@ -16,12 +19,57 @@ chmod +x ./configure
 $CONFIGURE --static
 $MAKE install CFLAGS="$FLAGS"
 
+# Build libxml
+cd ../libxml
+autoreconf -fiv
+$CONFIGURE --with-python=no --enable-static --disable-shared CFLAGS="$FLAGS"
+$MAKE install
+
 # Build libpng
 cd ../png
 autoreconf -fiv
-$CONFIGURE --disable-mips-msa --disable-arm-neon --disable-powerpc-vsx --disable-shared
-$MAKE install CFLAGS="$FLAGS"
+$CONFIGURE --disable-mips-msa --disable-arm-neon --disable-powerpc-vsx --disable-shared CFLAGS="$FLAGS"
+$MAKE install
 
+# Build libjpeg-turbo
+cd ../jpeg
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_SHARED=off ${SIMD_FLAG} -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
+$MAKE install
+
+# Build libtiff
+cd ../tiff
+autoreconf -fiv
+$CONFIGURE ${CONDITIONAL_DISABLE_SHARED} CFLAGS="$FLAGS"
+$MAKE install
+
+# Build libwebpmux/demux
+cd ../webp
+autoreconf -fiv
+$CONFIGURE --enable-libwebpmux --enable-libwebpdemux --disable-shared CFLAGS="${FLAGS}"
+$MAKE install
+
+# Build openjpeg
+cd ../openjpeg
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS" || true
+$CMAKE_CONFIGURE . -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
+$MAKE install
+cp bin/libopenjp2.a /usr/local/lib
+
+# Build lcms
+cd ../lcms
+autoreconf -fiv
+$CONFIGURE --disable-shared --prefix=/usr/local CFLAGS="$FLAGS"
+$MAKE install
+
+# Build libraw
+cd ../libraw
+chmod +x ./version.sh
+chmod +x ./shlib-version.sh
+autoreconf -fiv
+$CONFIGURE --disable-shared --disable-examples --disable-openmp --disable-jpeg --disable-jasper --prefix=/usr/local  CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
+$MAKE install
+
+# Build ImageMagick
 cd ../ImageMagick
-$CONFIGURE --disable-shared --disable-openmp --enable-static --enable-delegate-build --without-threads --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-quantum-depth=8 --disable-hdri PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
-$MAKE install CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS"
+$CONFIGURE --disable-shared --disable-openmp --enable-static --enable-delegate-build --without-threads --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-quantum-depth=8 --disable-hdri  CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS" PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
+$MAKE install
