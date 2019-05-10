e4bfabd15bc33e2d6d994354ec6138cb65640853
Dirk Lemstra <dirk@lemstra.org>
2019-05-10 07:54:08 +0200

    Minor refactoring

--- build/dotnet/linux/build.Native.sh
+++ build/dotnet/linux/build.Native.sh
@@ -8,5 +8,5 @@ echo "" > foo.cxx
 mkdir Q8
 cd Q8
 
-cmake -DDEPTH=8 -DHDRI_ENABLE=no -DQUANTUM_NAME=Q8 -DLIBRARY_NAME=Magick.NET-Q8-x64.Native.dll -DPLATFORM=LINUX ..
+cmake .. -DDEPTH=8 -DHDRI_ENABLE=no -DQUANTUM_NAME=Q8 -DLIBRARY_NAME=Magick.NET-Q8-x64.Native.dll -DPLATFORM=LINUX
 make
