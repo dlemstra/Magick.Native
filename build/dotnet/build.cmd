8dde91b6dc9d8adf275cbcb9bec4eb37401052d7
Dirk Lemstra <dirk@lemstra.org>
2019-05-26 15:05:02 +0200

    Remove script to copy the output.

--- build/dotnet/build.cmd
+++ build/dotnet/build.cmd
@@ -1,4 +1,3 @@
 @echo off
 
 docker build ../.. -f Dockerfile.dotnet -t magick-dotnet
-docker run -it -v %~dp0output:/output magick-dotnet /src/build/copy.output.sh
--- build/dotnet/linux/copy.output.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp /src/Native/Q8/libMagick.NET-Q8-x64.Native.dll.so /output
