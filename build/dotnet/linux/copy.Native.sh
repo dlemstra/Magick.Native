d5e7ba410e07af9587c27d7ec3fe55a6971553b6
Dirk Lemstra <dirk@lemstra.org>
2019-06-02 17:07:31 +0200

    Fixed mistake in copy script.

--- build/dotnet/linux/copy.Native.sh
+++ build/dotnet/linux/copy.Native.sh
@@ -5,7 +5,7 @@ copyNative() {
     local quantum=$1
     local target=$2
 
-    cp $quantum/libMagick.Native-$quantum-x64.dll.so $target/Magick.Native-Q16-HDRI-x64.dll.so
+    cp $quantum/libMagick.Native-$quantum-x64.dll.so $target/Magick.Native-$quantum-x64.dll.so
 }
 
 copyNative "Q8" $1
--- build/dotnet/macos/copy.Native.sh
+++ build/dotnet/macos/copy.Native.sh
@@ -5,7 +5,7 @@ copyNative() {
     local quantum=$1
     local target=$2
 
-    cp $quantum/libMagick.Native-$quantum-x64.dll.dylib $target/Magick.Native-Q16-HDRI-x64.dll.dylib
+    cp $quantum/libMagick.Native-$quantum-x64.dll.dylib $target/Magick.Native-$quantum-x64.dll.dylib
 }
 
 copyNative "Q8" $1
