6dc08f74b5f8fe806822f1eca3e3f6d186806519
Dirk Lemstra <dirk@lemstra.org>
2019-06-02 20:55:59 +0200

    Added script to publish the libraries.

---
+++ publish/dotnet/publish.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+powershell .\publish.ps1
---
+++ publish/dotnet/publish.ps1
@@ -0,0 +1,29 @@
+# Copyright 2013-2019 Dirk Lemstra <https://github.com/dlemstra/Magick.Native/>
+#
+# Licensed under the ImageMagick License (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+#
+#   https://www.imagemagick.org/script/license.php
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+. $PSScriptRoot\..\..\tools\windows\utils.ps1
+
+function createNuGetPackage($version)
+{
+  $path = FullPath "publish\Magick.Native.nuspec"
+  $xml = [xml](Get-Content $path)
+  $xml.package.metadata.version = $version
+
+  $nuspecFile = FullPath "publish\files\Magick.Native.nuspec"
+  $xml.Save($nuspecFile)
+
+  $nuget = FullPath "tools\windows\nuget.exe"
+  & $nuget pack $nuspecFile -NoPackageAnalysis
+}
+
+$version = [string](Get-Date -format "yyyy.MM.dd.HHmm")
+createNuGetPackage $version
