1430ce5efe39aaf957d7e39f38d8ec5ce6d01c5b
Dirk Lemstra <dirk@lemstra.org>
2019-06-08 11:49:10 +0200

    Use the same folder structure on Linux and macOS.

--- build/dotnet/linux/copy.Native.sh
+++ build/dotnet/linux/copy.Native.sh
@@ -5,7 +5,7 @@ copyNative() {
     local quantum=$1
     local target=$2
 
-    cp $quantum/libMagick.Native-$quantum-x64.dll.so $target/Magick.Native-$quantum-x64.dll.so
+    cp $quantum/libMagick.Native-$quantum-x64.dll.so $target/Release$quantum/x64/Magick.Native-$quantum-x64.dll.so
 }
 
 copyNative "Q8" $1
--- build/dotnet/macos/copy.Native.sh
+++ build/dotnet/macos/copy.Native.sh
@@ -5,7 +5,7 @@ copyNative() {
     local quantum=$1
     local target=$2
 
-    cp $quantum/libMagick.Native-$quantum-x64.dll.dylib $target/Magick.Native-$quantum-x64.dll.dylib
+    cp $quantum/libMagick.Native-$quantum-x64.dll.dylib $target/Release$quantum/x64/Magick.Native-$quantum-x64.dll.dylib
 }
 
 copyNative "Q8" $1
