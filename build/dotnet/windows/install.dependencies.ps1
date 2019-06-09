7030fb9717a3fd936e0b6703c8ee5845ea491f38
Dirk Lemstra <dirk@lemstra.org>
2019-06-09 07:28:58 +0200

    Sign the native library on Windows.

--- build/dotnet/windows/install.dependencies.ps1
+++ build/dotnet/windows/install.dependencies.ps1
@@ -10,6 +10,16 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
+param (
+    [string]$pfxUri = ''
+)
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $targetNugetExe = "$PSScriptRoot\..\..\..\tools\windows\nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+
+if ($pfxUri -ne '') {
+    Invoke-WebRequest $pfxUri -OutFile "$PSScriptRoot\ImageMagick.pfx"
+}
