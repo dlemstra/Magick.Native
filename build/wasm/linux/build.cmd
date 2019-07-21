e54d4948add73f0d0c1f3d614f8ac7e4b79d2be2
Dirk Lemstra <dirk@lemstra.org>
2019-07-21 15:03:54 +0200

    Renamed source folder.

--- build/wasm/linux/build.cmd
+++ build/wasm/linux/build.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 docker build ../../.. -f Dockerfile -t magick-wasm-linux
-docker run -it -v %~dp0output:/output -w /Native magick-wasm-linux /build/copy.Native.sh /output
+docker run -it -v %~dp0output:/output -w /Magick.Native magick-wasm-linux /build/copy.Native.sh /output
