fba775072fd040f6f50a3c97ef9ed1bbdcd98470
Dirk Lemstra <dirk@lemstra.org>
2019-08-26 23:30:12 +0200

    Create single JS file use a different module name and enabled modularize.

--- build/wasm/linux/copy.Native.sh
+++ build/wasm/linux/copy.Native.sh
@@ -6,7 +6,6 @@ copyNative() {
     local target=$2
 
     cp $quantum/magick-$quantum.js $target/magick-$quantum.js
-    cp $quantum/magick-$quantum.wasm $target/magick-$quantum.wasm
 }
 
 copyNative "Q8" $1
