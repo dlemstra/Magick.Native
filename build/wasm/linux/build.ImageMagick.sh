be667d563b6d1cb74799fa39b0cc19539cee700c
Dirk Lemstra <dirk@lemstra.org>
2019-10-24 23:03:01 +0200

    Added missing newline.

--- build/wasm/linux/build.ImageMagick.sh
+++ build/wasm/linux/build.ImageMagick.sh
@@ -44,6 +44,7 @@ mkdir build
 cd build
 $CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_SHARED=off -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE -DCMAKE_C_FLAGS="$FLAGS"
 $MAKE install
+
 cd ..
 # Build libjpeg-turbo
 cd ../jpeg
