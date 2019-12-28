14dcfc77517169d3ecb9e1d4d542a7934ba16536
Dirk Lemstra <dirk@lemstra.org>
2019-12-28 12:09:13 +0100

    Add GitHub actions build that builds the Linux binaries.

--- build/dotnet/linux/copy.Native.sh
+++ build/dotnet/linux/copy.Native.sh
@@ -10,6 +10,8 @@ copyNative() {
     cp $quantum/libMagick.Native-$quantum-x64.dll.so $target/Release$quantum/x64/Magick.Native-$quantum-x64.dll.so
 }
 
+[ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
+
 copyNative "Q8" $1
 copyNative "Q16" $1
 copyNative "Q16-HDRI" $1
