852384e26d7bd7722591c07fcebf3fe57e7ab963
Dirk Lemstra <dirk@lemstra.org>
2020-01-12 10:20:56 +0100

    Added some build scripts to build ImageMagick.

---
+++ src/ImageMagick/build/Build.cmd
@@ -0,0 +1,26 @@
+@echo off
+call "..\..\..\tools\windows\init.visualstudio.cmd"
+
+set config=%1
+set quantumName=%2
+set platformName=%3
+
+if "%config%"=="" goto invalid
+if "%quantumName%"=="" goto invalid
+if "%platformName%"=="" goto invalid
+
+cd ..\..\..\build\dotnet\windows\
+
+powershell .\build.ImageMagick.ps1 -config %config% -quantumName %quantumName% -platformName %platformName%
+
+goto done
+
+:invalid
+
+echo.
+echo Do not use this script directly.
+echo.
+
+:done
+
+pause
