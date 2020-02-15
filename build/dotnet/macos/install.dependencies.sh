ed6f11370cb17a31b63dd0ba2ca0d883ed826b2a
Dirk Lemstra <dirk@lemstra.org>
2020-02-15 07:43:16 +0100

    Uninstall other libraries

--- build/dotnet/macos/install.dependencies.sh
+++ build/dotnet/macos/install.dependencies.sh
@@ -6,5 +6,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 brew install autoconf automake libiconv libtool nasm ossp-uuid pkg-config
 
 # Uninstall already installed development libraries
+brew uninstall --force freetype
+brew uninstall --force webp
 brew uninstall --force libpng
 brew uninstall --ignore-dependencies xz
