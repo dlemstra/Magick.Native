b991cf6099bfeaa1d9d89b5644097fcdb029d68a
Dirk Lemstra <dirk@lemstra.org>
2020-06-30 23:17:19 +0200

    Try with a patched version of configure.ac

--- build/libraries/build.lqr.sh
+++ build/libraries/build.lqr.sh
@@ -2,8 +2,8 @@
 set -e
 
 cd lqr
-git reset b047fbfba816c9511824782dae3841cf622dbca3 --hard
-autoreconf -fiv || true
+git reset c0c7ce334c7643bb4128566053af27f3c85addf0 --hard
+autoreconf -fiv
 chmod +x ./configure
 mkdir /usr/local/include/lqr-1
 mkdir /usr/local/include/lqr-1/lqr
