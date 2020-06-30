1d4b3ca9ddb9a376830a0114a68497d051aee46c
Dirk Lemstra <dirk@lemstra.org>
2020-06-30 21:54:27 +0200

    Ignore exit code of autoreconf and make ./configure executable.

--- build/libraries/build.lqr.sh
+++ build/libraries/build.lqr.sh
@@ -3,7 +3,8 @@ set -e
 
 cd lqr
 git reset b047fbfba816c9511824782dae3841cf622dbca3 --hard
-autoreconf -fiv
+autoreconf -fiv || true
+chmod +x ./configure
 mkdir /usr/local/include/lqr-1
 mkdir /usr/local/include/lqr-1/lqr
 $CONFIGURE --prefix=/usr/local --disable-shared CFLAGS="$FLAGS"
