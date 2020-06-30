8f481d22bf85e6704cbe91d2cdd737e56b46532a
Dirk Lemstra <dirk@lemstra.org>
2020-06-30 13:04:08 +0200

    Remove option to fix building on MacOS.

--- build/libraries/build.glib.sh
+++ build/libraries/build.glib.sh
@@ -3,6 +3,6 @@ git reset 586514423cb4dc09876dad095641f5b44fb2683d --hard
 mkdir build
 cd build
 export CFLAGS="$FLAGS"
-meson --prefix=/usr/local --default-library=static -Diconv="libc" -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dnls=disabled -Dinternal_pcre=true -Dgtk_doc=false -Dman=false ..
+meson --prefix=/usr/local --default-library=static -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dnls=disabled -Dinternal_pcre=true -Dgtk_doc=false -Dman=false ..
 ninja install
 cp /usr/local/lib/x86_64-linux-gnu/libglib-2.0.a /usr/local/lib/libglib-2.0.a
