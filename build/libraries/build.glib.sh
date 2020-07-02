ad55a42a35951af5121bea3cf5280c858138c70d
Dirk Lemstra <dirk@lemstra.org>
2020-07-02 21:49:08 +0200

    Use updated version of glib.

--- build/libraries/build.glib.sh
+++ build/libraries/build.glib.sh
@@ -2,12 +2,18 @@
 set -e
 
 cd glib
-git reset 586514423cb4dc09876dad095641f5b44fb2683d --hard
+git reset 407b61cd1eefdc4945b485849d5ee0709e55524c --hard
+
+rm glib/config.h
+rm glib/glibconfig.h
+rm gmodule/gmoduleconf.h
+
 mkdir build
 cd build
 export CFLAGS="$FLAGS"
 meson --prefix=/usr/local --default-library=static -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dnls=disabled -Dinternal_pcre=true -Dgtk_doc=false -Dman=false ..
 ninja install
+
 if [ -f /usr/local/lib/x86_64-linux-gnu/libglib-2.0.a ]; then
   cp /usr/local/lib/x86_64-linux-gnu/libglib-2.0.a /usr/local/lib/libglib-2.0.a
 fi
