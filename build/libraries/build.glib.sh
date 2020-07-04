59c2166ff3449a6563c30a1699af4c7b5a5d302d
Dirk Lemstra <dirk@lemstra.org>
2020-07-04 08:51:56 +0200

    More tweaks to building glib on MacOS.

--- build/libraries/build.glib.sh
+++ build/libraries/build.glib.sh
@@ -2,11 +2,16 @@
 set -e
 
 cd glib
-git reset 407b61cd1eefdc4945b485849d5ee0709e55524c --hard
+git reset 6caafa6ce0f85fc95a5238b564170e28be8d5223 --hard
 
+rm gio/gioenumtypes.c
+rm gio/gioenumtypes.h
+rm gio/gnetworking.h
 rm glib/config.h
 rm glib/glibconfig.h
+rm glib/gnulib/gnulib_math.h
 rm gmodule/gmoduleconf.h
+rm gobject/glib-enumtypes.h
 
 mkdir build
 cd build
