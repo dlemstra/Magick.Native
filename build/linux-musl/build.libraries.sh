ff0bdcc7576343268b8d7b083dc0c6204c90a6ee
Dirk Lemstra <dirk@lemstra.org>
2020-09-21 21:04:18 +0200

    Move lib64 earlier to make sure that pkgconfig can find them.

--- build/linux-musl/build.libraries.sh
+++ build/linux-musl/build.libraries.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
+moveFromLib64() {
+    mv /usr/local/lib64/pkgconfig/*.pc /usr/local/lib/pkgconfig
+    mv /usr/local/lib64/*.a /usr/local/lib
+}
+
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 . $SCRIPT_PATH/settings.sh
@@ -10,15 +16,18 @@ $1/build.libpng.sh
 $1/build.freetype.sh
 $1/build.fontconfig.sh
 $1/build.libjpeg-turbo.sh
+moveFromLib64
 $1/build.libtiff.sh
 $1/build.libwebp.sh
 $1/build.openjpeg.sh
 $1/build.lcms.sh
 $1/build.libde265.sh
 $1/build.aom.sh
+moveFromLib64
 $1/build.libheif.sh
 $1/build.libraw.sh
 $1/build.openexr.sh
+moveFromLib64
 $1/build.ffi.sh
 $1/build.glib.sh
 $1/build.lqr.sh
@@ -29,4 +38,3 @@ $1/build.harfbuzz.sh
 $1/build.pango.sh
 $1/build.croco.sh
 $1/build.librsvg.sh
-cp /usr/local/lib64/*.a /usr/local/lib
