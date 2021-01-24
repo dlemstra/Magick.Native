f5bdd3502d1ab713a7af914b8df1529eec056c50
Dirk Lemstra <dirk@lemstra.org>
2021-01-24 18:27:01 +0100

    Removed echo statement.

--- build/linux-openmp/build.ImageMagick.sh
+++ build/linux-openmp/build.ImageMagick.sh
@@ -18,7 +18,6 @@ buildImageMagick() {
         hdri=yes
     fi
 
-    echo $CONFIGURE --disable-shared --disable-opencl --enable-static --enable-delegate-build --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-rsvg --with-jxl --with-quantum-depth=$depth --enable-hdri=$hdri $IMAGEMAGICK_OPTIONS CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS" PKG_CONFIG_PATH="$PKG_PATH"
     $CONFIGURE --disable-shared --disable-opencl --enable-static --enable-delegate-build --without-magick-plus-plus --without-utilities --disable-docs --without-bzlib --without-lzma --without-x --with-rsvg --with-jxl --with-quantum-depth=$depth --enable-hdri=$hdri $IMAGEMAGICK_OPTIONS CFLAGS="$STRICT_FLAGS" CXXFLAGS="$STRICT_FLAGS" PKG_CONFIG_PATH="$PKG_PATH"
     $MAKE install
 }
