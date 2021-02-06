5b151c76e4f50504dcf27ac7b14ebe7cb7a56245
Dirk Lemstra <dirk@lemstra.org>
2021-02-06 12:47:10 +0100

    Also install texinfo.

--- build/linux-musl/install.dependencies.sh
+++ build/linux-musl/install.dependencies.sh
@@ -3,4 +3,4 @@ set -e
 
 apk update
 
-apk add autoconf automake cmake file g++ gcc gettext-tiny git gperf libtool make meson musl-libintl nasm pkgconf python3 py3-lxml py3-pip ragel
+apk add autoconf automake cmake file g++ gcc gettext-tiny git gperf libtool make meson musl-libintl nasm pkgconf python3 py3-lxml py3-pip ragel texinfo
