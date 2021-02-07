b909de20596e1b1962f4c911012bb9c549c1e7b0
Dirk Lemstra <dirk@lemstra.org>
2021-02-07 18:51:34 +0100

    Also remove libzip.

--- build/macos/install.dependencies.sh
+++ build/macos/install.dependencies.sh
@@ -19,6 +19,7 @@ brew uninstall --force sox
 brew uninstall --force libpng
 brew uninstall --force curl-openssl
 brew uninstall --force zstd
+brew uninstall --force libzip
 brew uninstall --ignore-dependencies xz
 
 pip3 install meson==0.55.3
