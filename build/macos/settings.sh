d37d6a52459fc664e328145fa7f6432a03f5bda7
Dirk Lemstra <dirk@lemstra.org>
2021-02-28 13:29:04 +0100

    Added another option.

--- build/macos/settings.sh
+++ build/macos/settings.sh
@@ -1,4 +1,4 @@
-export FLAGS="-O3 -fPIC -mmacosx-version-min=10.3"
+export FLAGS="-O3 -fPIC -mmacosx-version-min=10.3 -stdlib=libc++"
 export STRICT_FLAGS="${FLAGS} -Wall"
 export CONFIGURE="./configure"
 export CMAKE_COMMAND="cmake"
