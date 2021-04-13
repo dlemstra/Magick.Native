254d651fb0f67bdd7f23f52a0355c5445845d084
Dirk Lemstra <dirk@lemstra.org>
2021-04-13 21:55:46 +0200

    Try if setting GTKDOCIZE works.

--- build/libraries/build.cairo.sh
+++ build/libraries/build.cairo.sh
@@ -5,6 +5,8 @@ cd cairo
 
 rm src/cairo-features.h
 
+export GTKDOCIZE=true
+
 autoreconf -fiv
 chmod +x ./configure
 $CONFIGURE --disable-shared --enable-quartz=no --prefix=/usr/local CFLAGS="$FLAGS"
