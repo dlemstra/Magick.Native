f8f8e1253f308192d3a34b824460a692214eecfa
Dirk Lemstra <dirk@lemstra.org>
2021-08-21 16:41:54 +0200

    The patch is no longer necessary.

--- build/wasm/patches/install.patches.sh
+++ build/wasm/patches/install.patches.sh
@@ -3,11 +3,7 @@ set -e
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd freetype
-git apply $SCRIPT_PATH/freetype-freetype.mk.patch
-git apply $SCRIPT_PATH/freetype-unix-cc.in.patch
-
-cd ../png
+cd png
 git apply $SCRIPT_PATH/png.patch
 
 cd ../libde265
