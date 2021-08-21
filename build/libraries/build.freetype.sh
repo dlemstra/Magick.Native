ecd0139e33add1a0844f8bc9090dad800bcc795d
Dirk Lemstra <dirk@lemstra.org>
2021-08-21 16:41:17 +0200

    Use cmake for freetype instead.

--- build/libraries/build.freetype.sh
+++ build/libraries/build.freetype.sh
@@ -2,6 +2,7 @@
 set -e
 
 cd freetype
-./autogen.sh
-$CONFIGURE --disable-shared --without-bzip2 CFLAGS="$FLAGS"
+mkdir __build
+cd __build
+$CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
 $MAKE install
