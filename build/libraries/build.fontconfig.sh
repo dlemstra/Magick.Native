da9e3f499111e62fecd1eb6ddc9ad2654bc178f3
Dirk Lemstra <dirk@lemstra.org>
2021-09-05 13:17:39 +0200

    Make file executable.

--- build/libraries/build.fontconfig.sh
+++ build/libraries/build.fontconfig.sh
@@ -3,6 +3,7 @@ set -e
 
 cd fontconfig
 autoreconf -fiv
+chmod +x ./configure
 pip install lxml
 pip install six
 $CONFIGURE --enable-libxml2 --enable-static=yes --disable-shared $FONTCONFIG_OPTIONS CFLAGS="$FLAGS"
