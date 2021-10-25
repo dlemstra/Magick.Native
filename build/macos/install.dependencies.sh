659bea8759f8736dbc3274a5505989b392fdbfa9
Dirk Lemstra <dirk@lemstra.org>
2021-10-25 19:27:01 +0200

    Ignore dependencies when removing brotli.

--- build/macos/install.dependencies.sh
+++ build/macos/install.dependencies.sh
@@ -28,7 +28,7 @@ brew uninstall --force libpng
 brew uninstall --force curl-openssl
 brew uninstall --force libzip
 brew uninstall --force httpd
-brew uninstall --force brotli
+brew uninstall --ignore-dependencies brotli
 brew uninstall --ignore-dependencies libidn2
 brew uninstall --ignore-dependencies xz
 brew uninstall --ignore-dependencies zstd
