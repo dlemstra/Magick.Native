5777d044ea8443582125e7151d40d224eae9c2af
Dirk Lemstra <dirk@lemstra.org>
2022-03-08 19:38:50 +0100

    Added comments to settings files.

--- build/linux-musl/settings.sh
+++ build/linux-musl/settings.sh
@@ -1,3 +1,4 @@
+# Shared options
 export FLAGS="-O3 -fPIC"
 export STRICT_FLAGS="${FLAGS} -Wall"
 export CONFIGURE="./configure"
@@ -10,6 +11,7 @@ export PKG_PATH="/usr/local/lib/pkgconfig"
 export SIMD_OPTIONS="-DWITH_SIMD=1"
 export SSE_OPTIONS=""
 
+# Library specific options
 export FFI_OPTIONS="--disable-exec-static-tramp"
 export FONTCONFIG_OPTIONS=""
 export GLIB_OPTIONS=""
@@ -17,4 +19,5 @@ export LIBXML_OPTIONS=""
 export OPENEXR_OPTIONS=""
 export WEBP_OPTIONS="--enable-libwebpmux --enable-libwebpdemux"
 
+# ImageMagick options
 export IMAGEMAGICK_OPTIONS="--disable-openmp"
--- build/wasm/settings.sh
+++ build/wasm/settings.sh
@@ -1,3 +1,4 @@
+# Shared options
 export FLAGS="-O3"
 export STRICT_FLAGS="${FLAGS} -Wall"
 export CONFIGURE="emconfigure ./configure"
@@ -10,6 +11,7 @@ export PKG_PATH="/usr/local/lib/pkgconfig"
 export SIMD_OPTIONS="-DWITH_SIMD=0"
 export SSE_OPTIONS="--disable-sse"
 
+# Library specific options
 export FFI_OPTIONS="--disable-exec-static-tramp"
 export FONTCONFIG_OPTIONS=""
 export GLIB_OPTIONS="--cross-file=../emscripten-crossfile.meson"
@@ -17,6 +19,8 @@ export LIBXML_OPTIONS=""
 export OPENEXR_OPTIONS="-DZLIB_LIBRARY=/usr/local/lib/libz.a -DZLIB_INCLUDE_DIR=/usr/local/include -DImath_DIR=/usr/local/lib/cmake/Imath"
 export WEBP_OPTIONS=""
 
+# ImageMagick options
 export IMAGEMAGICK_OPTIONS="--disable-openmp --without-threads --without-bzlib --without-lzma"
 
+# WASM options
 export EM_PKG_CONFIG_PATH=$PKG_PATH
