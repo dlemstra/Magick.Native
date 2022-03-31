983cb0daa7024d0af40b814c2a32f983f45eddba
Dirk Lemstra <dirk@lemstra.org>
2022-03-31 13:47:15 +0200

    Use ubuntu 18.04 when building linux-arm64 instead.

--- build/linux-arm64/install.dependencies.sh
+++ build/linux-arm64/install.dependencies.sh
@@ -3,13 +3,7 @@ set -e
 
 apt-get update -y
 
-apt-get install -y autoconf autopoint gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel software-properties-common texinfo
-
-add-apt-repository ppa:ubuntu-toolchain-r/test -y
-apt-get update -y
-apt-get install binutils-aarch64-linux-gnu gcc-7 gcc-aarch64-linux-gnu g++-7 g++-aarch64-linux-gnu -y
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-update-alternatives --config gcc
+apt-get install -y autoconf autopoint binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel texinfo
 
 pip3 install cmake
 pip3 install meson==0.55.3
