d06fcb865a606b00ce45997a3c36bc18bbac1e35
Dirk Lemstra <dirk@lemstra.org>
2022-05-23 07:06:55 +0200

    Uninstall more packages.

--- build/macos/install.dependencies.sh
+++ build/macos/install.dependencies.sh
@@ -12,7 +12,11 @@ brew uninstall --force composer
 brew uninstall --force php
 brew uninstall --force gd
 brew uninstall --force r
+brew uninstall --force imagemagick
 brew uninstall --force cairo
+brew uninstall --force shared-mime-info
+brew uninstall --force libheif
+brew uninstall --force liblqr
 brew uninstall --force glib
 brew uninstall --force gnupg
 brew uninstall --force gnutls
