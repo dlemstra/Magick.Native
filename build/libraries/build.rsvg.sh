601db06d2e4e78d2f251453bc2a400514d2fb01a
Dirk Lemstra <dirk@lemstra.org>
2022-05-26 11:01:42 +0200

    Corrected build script.

--- build/libraries/build.rsvg.sh
+++ build/libraries/build.rsvg.sh
@@ -6,17 +6,15 @@ cd rsvg
 export GLIB_MKENUMS="/usr/local/bin/glib-mkenums"
 export GLIB_GENMARSHAL="/usr/local/bin/glib-genmarshal"
 
-rm gdk-pixbuf/gdk-pixbuf/config.h
-rm config.h
-
 cd gdk-pixbuf
-
+rm gdk-pixbuf/config.h
 autoreconf -fiv
 chmod +x ./configure
 $CONFIGURE $CONFIGURE_OPTIONS --disable-shared --with-included-loaders=yes --enable-modules=no --prefix=/usr/local CFLAGS="$FLAGS"
 $MAKE install
-cd ..
 
+cd ../librsvg
+rm config.h
 autoreconf -fiv
 chmod +x ./configure
 $CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-Bsymbolic --disable-introspection --disable-pixbuf-loader --prefix=/usr/local CFLAGS="$FLAGS -I/usr/local/include/libcroco-0.6/libcroco"
