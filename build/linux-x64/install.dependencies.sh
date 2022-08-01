aec65b71799801f58822bebd09d249b9a1e09ba5
Dirk Lemstra <dirk@lemstra.org>
2022-08-01 23:35:10 +0200

    Use ubuntu 18.04 when building linux-x64 instead.

--- build/linux-x64/install.dependencies.sh
+++ build/linux-x64/install.dependencies.sh
@@ -3,14 +3,9 @@ set -e
 
 apt-get update -y
 
-apt-get install -y autoconf autopoint gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel software-properties-common texinfo
+apt-get install -y autoconf autopoint binutils gcc g++ gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel texinfo
 
-add-apt-repository ppa:ubuntu-toolchain-r/test -y
-apt-get update -y
-apt-get install gcc-7 g++-7 -y
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-update-alternatives --config gcc
-
+pip3 install --upgrade pip
 pip3 install cmake
 pip3 install meson==0.55.3
 pip3 install ninja==1.10.0.post2
