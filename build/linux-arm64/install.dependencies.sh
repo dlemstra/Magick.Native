d81702df068f2a23dda943c17382e52e32d18231
Dirk Lemstra <dirk@lemstra.org>
2022-08-01 23:16:30 +0200

    Fixed build error by upgrading pip.

--- build/linux-arm64/install.dependencies.sh
+++ build/linux-arm64/install.dependencies.sh
@@ -5,6 +5,7 @@ apt-get update -y
 
 apt-get install -y autoconf autopoint binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel texinfo
 
+pip3 install --upgrade pip
 pip3 install cmake
 pip3 install meson==0.55.3
 pip3 install ninja==1.10.0.post2
