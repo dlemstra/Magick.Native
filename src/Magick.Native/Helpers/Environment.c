b6915e989cf38e146ddfb6c9d3133583d354437b
Dirk Lemstra <dirk@lemstra.org>
2022-08-07 12:59:52 +0200

    Also use wide char in SetEnv.

--- src/Magick.Native/Helpers/Environment.c
+++ src/Magick.Native/Helpers/Environment.c
@@ -5,6 +5,23 @@
 #include "Environment.h"
 #include <stdlib.h>
 
+static inline wchar_t *create_wchar_string(const char *utf8)
+{
+  int
+    count;
+
+  wchar_t
+    *wide;
+
+  count = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+  if (count == 0)
+    return (wchar_t *) NULL;
+  wide = (wchar_t *) NTAcquireQuantumMemory(count, sizeof(*wide));
+  if (wide != (wchar_t *) NULL && MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wide, count) == 0)
+    wide = (wchar_t *) RelinquishMagickMemory(wide);
+  return wide;
+}
+
 MAGICK_NATIVE_EXPORT void Environment_Initialize(void)
 {
   MagickCoreGenesis((const char *) NULL, MagickFalse);
@@ -23,10 +40,7 @@ MAGICK_NATIVE_EXPORT char *Environment_GetEnv(const char *name)
     *wide_name,
     *wide_value;
 
-  count = (size_t) MultiByteToWideChar(CP_UTF8, 0, name, -1, NULL, 0);
-  if (count <= 0)
-    return (char *) NULL;
-  wide_name = (wchar_t *) AcquireQuantumMemory(count, sizeof(*wide_name));
+  wide_name = create_wchar_string(name);
   if (wide_name == (wchar_t *) NULL)
     return (char *) NULL;
   (void) _wgetenv_s(&count, NULL, 0, wide_name);
@@ -65,7 +79,16 @@ MAGICK_NATIVE_EXPORT char *Environment_GetEnv(const char *name)
 MAGICK_NATIVE_EXPORT void Environment_SetEnv(const char *name, const char *value)
 {
 #if defined(MAGICK_NATIVE_WINDOWS)
-  _putenv_s(name, value);
+  wchar_t
+    *wide_name,
+    *wide_value;
+
+  wide_name = create_wchar_string(name);
+  wide_value = create_wchar_string(value);
+  if (wide_name != (wchar_t *) NULL && wide_value != (wchar_t *) NULL)
+    _wputenv_s(wide_name, wide_value);
+  wide_name = (wchar_t *) RelinquishMagickMemory(wide_name);
+  wide_value = (wchar_t *) RelinquishMagickMemory(wide_value);
 #else
   (void) setenv(name, value, 1);
 #endif
