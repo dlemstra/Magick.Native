828a62ff8c446616a9c2b6f51174d2b16a349157
Dirk Lemstra <dirk@lemstra.org>
2022-08-14 08:44:10 +0200

    Disable options in WebP.

--- build/libraries/build.webp.sh
+++ build/libraries/build.webp.sh
@@ -4,5 +4,5 @@ set -e
 cd webp
 autoreconf -fiv
 chmod +x ./configure
-$CONFIGURE $CONFIGURE_OPTIONS $WEBP_OPTIONS --disable-shared CFLAGS="$FLAGS"
+$CONFIGURE $CONFIGURE_OPTIONS $WEBP_OPTIONS --disable-shared --disable-png --disable-jpeg --disable-tiff CFLAGS="$FLAGS"
 $MAKE install
