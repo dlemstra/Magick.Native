bbf2ec88c2056d00b76e2b2b489e1769a0d4acae
Dirk Lemstra <dirk@lemstra.org>
2022-09-02 15:03:33 +0200

    Use new version of cmake.

--- build/linux-musl-x64/install.dependencies.sh
+++ build/linux-musl-x64/install.dependencies.sh
@@ -4,3 +4,6 @@ set -e
 apk update
 
 apk add autoconf automake bash cmake file g++ gcc gettext-tiny git gperf libtool make meson musl-libintl nasm pkgconf python3 py3-lxml py3-pip ragel texinfo
+
+pip3 install --upgrade pip
+pip3 install cmake
