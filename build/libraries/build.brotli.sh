340dc2367e16c59180b71c7464e5efe67f1fb229
Dirk Lemstra <dirk@lemstra.org>
2022-09-02 14:54:12 +0200

    Changes due to upgrade of jpeg-xl.

--- build/libraries/build.brotli.sh
+++ build/libraries/build.brotli.sh
@@ -6,3 +6,8 @@ mkdir __build
 cd __build
 $CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=false -DBROTLI_DISABLE_TESTS=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS -DBROTLI_EXTERNAL_DICTIONARY_DATA"
 $MAKE install
+
+# need this for jpeg-xl
+ln -s /usr/local/lib/libbrotlicommon-static.a /usr/local/lib/libbrotlicommon.a
+ln -s /usr/local/lib/libbrotlidec-static.a /usr/local/lib/libbrotlidec.a
+ln -s /usr/local/lib/libbrotlienc-static.a /usr/local/lib/libbrotlienc.a
