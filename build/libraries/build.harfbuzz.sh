77211a88f12000d2a564c59d63b2bcd89091ef45
Alastair Porter <alastair@porter.net.nz>
2022-10-03 12:29:57 +0200

    Fix build flags for meson-based dependencies
    To set flags to the C compiler when using meson, use CFLAGS instead of
    C_FLAGS.
    Harfbuzz is written in C++, and so require CXXFLAGS.

--- build/libraries/build.harfbuzz.sh
+++ build/libraries/build.harfbuzz.sh
@@ -4,5 +4,5 @@ set -e
 cd harfbuzz
 mkdir __build
 cd __build
-CFLAGS=$FLAGS meson .. $MESON_OPTIONS --prefix=/usr/local --default-library=static -Ddocs=disabled -Dtests=disabled -Dcairo=disabled
+CXXFLAGS=$FLAGS meson .. $MESON_OPTIONS --prefix=/usr/local --default-library=static -Ddocs=disabled -Dtests=disabled -Dcairo=disabled
 ninja install
