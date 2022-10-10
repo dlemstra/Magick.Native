69c45cd8989945a649d8dba2c4b3d97d2977ba12
Alastair Porter <alastair@porter.net.nz>
2022-10-10 17:53:46 +0200

    Compile apple silicon by cross-compiling on an x86_64 host

--- build/libraries/build.tiff.sh
+++ build/libraries/build.tiff.sh
@@ -3,6 +3,6 @@ set -e
 
 cd tiff
 autoreconf -fiv
-$CONFIGURE $CONFIGURE_OPTIONS --disable-shared CFLAGS="$FLAGS"
+$CONFIGURE $CONFIGURE_OPTIONS --disable-shared CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
 echo '' > version
 $MAKE install
--- build/shared/build.Native.sh
+++ build/shared/build.Native.sh
@@ -38,7 +38,7 @@ buildNative() {
     mkdir $quantum
     cd $quantum
 
-    $CMAKE_COMMAND -DDEPTH=$depth -DHDRI_ENABLE=$hdri_enable -DQUANTUM_NAME=$quantum_name -DLIBRARY_NAME=$library_name -DPLATFORM=$PLATFORM ..
+    $CMAKE_COMMAND $CMAKE_OPTIONS -DDEPTH=$depth -DHDRI_ENABLE=$hdri_enable -DQUANTUM_NAME=$quantum_name -DLIBRARY_NAME=$library_name -DPLATFORM=$PLATFORM ..
     make
 
     cd ..
