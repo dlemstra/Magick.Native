3fb60d05e7cabf6b55848fb16704067c0e114f0a
Dirk Lemstra <dirk@lemstra.org>
2022-10-11 22:06:48 +0200

    Disable xlib with cairo.

--- build/libraries/build.cairo.sh
+++ build/libraries/build.cairo.sh
@@ -7,5 +7,5 @@ rm src/cairo-features.h
 
 autoreconf -fiv
 chmod +x ./configure
-$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --enable-quartz=no --prefix=/usr/local CFLAGS="$FLAGS"
+$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-xlib --enable-quartz=no --prefix=/usr/local CFLAGS="$FLAGS"
 $MAKE install
