0df62b23ca9b1030451876cfe8bcd938a63e78dc
Dirk Lemstra <dirk@lemstra.org>
2022-10-15 12:46:53 +0200

    Changed CMAKE_BUILD_TYPE option into a setting.

--- build/libraries/build.zip.sh
+++ build/libraries/build.zip.sh
@@ -7,5 +7,5 @@ rm lib/zip_err_str.c
 rm lib/zipconf.h
 mkdir __build
 cd __build
-$CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="$FLAGS"
+$CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_C_FLAGS="$FLAGS"
 $MAKE install
