c3000662fcf047f80dfd5984a10d3a37d4f07a2c
Dirk Lemstra <dirk@lemstra.org>
2022-10-16 16:31:40 +0200

    Switch to pip3.

--- build/libraries/build.fontconfig.sh
+++ build/libraries/build.fontconfig.sh
@@ -4,7 +4,7 @@ set -e
 cd fontconfig
 autoreconf -fiv
 chmod +x ./configure
-pip install lxml
-pip install six
+pip3 install lxml
+pip3 install six
 $CONFIGURE $CONFIGURE_OPTIONS --enable-libxml2 --enable-static=yes --disable-shared $FONTCONFIG_OPTIONS CFLAGS="$FLAGS"
 $MAKE install
