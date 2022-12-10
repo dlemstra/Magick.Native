2901ee1b09ab60837ed7993e08918f82ed8380a4
Dirk Lemstra <dirk@lemstra.org>
2022-12-10 21:19:41 +0100

    Revert "Moved installation of dependencies for verify to a separate script."

--- build/linux-musl-x64/verify.openmp.cmd
+++ build/linux-musl-x64/verify.openmp.cmd
@@ -2,4 +2,4 @@
 
 set image=alpine:latest
 
-docker run -v %~dp0../:/build %image% /bin/sh -c "apk update && apk add bash && /build/shared/install.verify.dependencies.sh && /build/shared/verify.Native.sh linux-musl x64 /build/linux-musl-x64/output OpenMP"
+docker run -v %~dp0../:/build %image% /bin/sh -c "apk update && apk add bash && /build/shared/verify.Native.sh linux-musl x64 /build/linux-musl-x64/output OpenMP"
