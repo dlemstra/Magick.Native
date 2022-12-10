0d608597204eda34dc4542113f723d2f5c522bdc
Dirk Lemstra <dirk@lemstra.org>
2022-12-10 11:10:27 +0100

    Link with older version of glibc.

--- src/Magick.Native/Stdafx.c
+++ src/Magick.Native/Stdafx.c
@@ -2,3 +2,27 @@
 // Licensed under the Apache License, Version 2.0.
 
 #include "Stdafx.h"
+
+#ifdef MAGICK_NATIVE_LINUX
+asm(".symver old_powf, powf@GLIBC_2.2.5");
+float old_powf(float base, float exponent);
+float powf(float base, float exponent)
+{
+  return old_powf(base, exponent);
+}
+
+asm(".symver old_logf, logf@GLIBC_2.2.5");
+float old_logf(float arg);
+float logf(float arg)
+{
+  return old_logf(arg);
+}
+
+asm(".symver old_log2f, log2f@GLIBC_2.2.5");
+float old_log2f(float arg);
+float log2f(float arg)
+{
+  return old_log2f(arg);
+}
+
+#endif
