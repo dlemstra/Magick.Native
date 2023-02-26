330ce8b777cf0e02b7dc3a82d334557915d074db
Dirk Lemstra <dirk@lemstra.org>
2023-02-26 15:42:28 +0100

    Style changes.

--- build/libraries/build.zip.sh
+++ build/libraries/build.zip.sh
@@ -2,9 +2,11 @@
 set -e
 
 cd zip
+
 rm lib/config.h
 rm lib/zip_err_str.c
 rm lib/zipconf.h
+
 mkdir __build
 cd __build
 $CMAKE_COMMAND .. -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_C_FLAGS="$FLAGS"
