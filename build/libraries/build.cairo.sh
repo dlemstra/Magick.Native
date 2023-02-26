a2e9323bcafc50a4b69b1a62690792485537f78d
Dirk Lemstra <dirk@lemstra.org>
2023-02-26 15:41:25 +0100

    Cairo now uses meson instead.

--- build/libraries/build.cairo.sh
+++ build/libraries/build.cairo.sh
@@ -4,8 +4,12 @@ set -e
 cd cairo
 
 rm src/cairo-features.h
+rm src/config.h
 
-autoreconf -fiv
-chmod +x ./configure
-$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-xlib --enable-quartz=no --prefix=/usr/local CFLAGS="$FLAGS"
-$MAKE install
+mkdir __build
+cd __build
+CXXFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dxlib=disabled -Dquartz=disabled -Dtests=disabled
+
+sed -i 's/#define CAIRO_HAS_PTHREAD 1/#define CAIRO_NO_MUTEX 1/' config.h
+
+ninja install
