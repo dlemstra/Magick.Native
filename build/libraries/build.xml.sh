53a246d8588de25a5ee493bd92726e5c8a64a065
Dirk Lemstra <dirk@lemstra.org>
2023-02-26 16:06:23 +0100

    Remove space after -i with sed to try and fix the MacOS build.

--- build/libraries/build.xml.sh
+++ build/libraries/build.xml.sh
@@ -3,7 +3,7 @@ set -e
 
 cd xml
 
-sed -i 's/1.16.3 //' configure.ac
+sed -i's/1.16.3 //' configure.ac
 
 autoreconf -fiv
 $CONFIGURE $CONFIGURE_OPTIONS --with-python=no --enable-static --disable-shared $XML_OPTIONS CFLAGS="$FLAGS"
