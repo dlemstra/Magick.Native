dc408445bb2c9f9540b4d05e807e6b29e134d41f
Dirk Lemstra <dirk@lemstra.org>
2023-02-26 22:25:25 +0100

    Another attempt to fix the arm64 macOS build.

--- build/libraries/build.pixman.sh
+++ build/libraries/build.pixman.sh
@@ -4,5 +4,5 @@ set -e
 cd pixman
 autoreconf -fiv
 chmod +x ./configure
-$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-arm-neon --prefix=/usr/local CFLAGS="$FLAGS"
+$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-arm-neon --disable-arm-a64-neon --prefix=/usr/local CFLAGS="$FLAGS"
 $MAKE install
