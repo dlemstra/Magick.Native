53489b6fdb7bb7a205886b4a976fc13c0761ba5d
Dirk Lemstra <dirk@lemstra.org>
2023-02-26 15:43:17 +0100

    Allow a lower version of automake.

--- build/libraries/build.xml.sh
+++ build/libraries/build.xml.sh
@@ -2,6 +2,9 @@
 set -e
 
 cd xml
+
+sed -i 's/1.16.3 //' configure.ac
+
 autoreconf -fiv
 $CONFIGURE $CONFIGURE_OPTIONS --with-python=no --enable-static --disable-shared $XML_OPTIONS CFLAGS="$FLAGS"
 $MAKE install
