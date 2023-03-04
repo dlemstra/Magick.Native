73c9120e2137eaf9da188e54878b0e22ae5c2eb1
Dirk Lemstra <dirk@lemstra.org>
2023-03-04 18:48:41 +0100

    Fix for the arm64 macOS build.

--- build/libraries/build.cairo.sh
+++ build/libraries/build.cairo.sh
@@ -8,7 +8,7 @@ rm src/config.h
 
 mkdir __build
 cd __build
-CXXFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dxlib=disabled -Dquartz=disabled -Dtests=disabled
+CFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dxlib=disabled -Dquartz=disabled -Dtests=disabled
 
 sed -i 's/#define CAIRO_HAS_PTHREAD 1/#define CAIRO_NO_MUTEX 1/' config.h
 
