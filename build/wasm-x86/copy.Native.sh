5517fdfa02bd4e458c78690d542c00a9b1188f7f
Dirk Lemstra <dirk@lemstra.org>
2023-04-20 12:08:15 +0200

    Also copy the wasm file and patch the js file to include the correct filename.

--- build/wasm-x86/copy.Native.sh
+++ build/wasm-x86/copy.Native.sh
@@ -4,4 +4,7 @@ set -e
 [ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
 
 cp Q8/Magick.Native-Q8-x86.dll.js $1/magick.js
-cp Q8/magick-Q8.d.ts $1/magick.d.ts
+cp Q8/Magick.Native-Q8-x86.dll.wasm $1/magick.wasm
+cp Q8/Magick.Native-Q8-x86.dll.d.ts $1/magick.d.ts
+
+sed -i 's/Magick.Native-Q8-x86.dll/magick/g' $1/magick.js
