e9176ea90a72faf35cc814f3aa501287ebff9762
Dirk Lemstra <dirk@lemstra.org>
2023-04-29 11:06:59 +0200

    Disable nls in the fontconfig build.

--- build/libraries/build.fontconfig.sh
+++ build/libraries/build.fontconfig.sh
@@ -6,5 +6,5 @@ autoreconf -fiv
 chmod +x ./configure
 pip3 install lxml
 pip3 install six
-$CONFIGURE $CONFIGURE_OPTIONS --enable-libxml2 --enable-static=yes --disable-shared $FONTCONFIG_OPTIONS CFLAGS="$FLAGS"
+$CONFIGURE $CONFIGURE_OPTIONS --enable-libxml2 --enable-static=yes --disable-nls --disable-shared $FONTCONFIG_OPTIONS CFLAGS="$FLAGS"
 $MAKE install
