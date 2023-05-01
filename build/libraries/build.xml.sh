245a0dc6474bc54021887689c55a2593037beaab
Dirk Lemstra <dirk@lemstra.org>
2023-05-01 19:44:10 +0200

    Remove files that were created with cmake.

--- build/libraries/build.xml.sh
+++ build/libraries/build.xml.sh
@@ -5,6 +5,9 @@ cd xml
 
 sed -i 's/1.16.3 //' configure.ac
 
+rm -Rf config.h
+rm -Rf include/libxml/xmlversion.h
+
 autoreconf -fiv
 $CONFIGURE $CONFIGURE_OPTIONS --with-python=no --enable-static --disable-shared $XML_OPTIONS CFLAGS="$FLAGS"
 $MAKE install
