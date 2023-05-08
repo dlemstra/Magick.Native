230c839cc83fa9cf057cbb3af20d0aaabb3d9e10
Dirk Lemstra <dirk@lemstra.org>
2023-05-08 22:09:34 +0200

    Removed files that were generated by cmake.

--- build/libraries/build.tiff.sh
+++ build/libraries/build.tiff.sh
@@ -2,6 +2,10 @@
 set -e
 
 cd tiff
+
+rm libtiff/tif_config.h
+rm libtiff/tiffconf.h
+
 autoreconf -fiv
 $CONFIGURE $CONFIGURE_OPTIONS --disable-shared CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
 echo '' > version
