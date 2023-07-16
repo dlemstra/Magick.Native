79677ae86733708f985b9d807374dfcd4f7dfb9f
Dirk Lemstra <dirk@lemstra.org>
2023-07-16 08:57:34 +0200

    Silenced warnings

--- src/Magick.Native/Stdafx.h
+++ src/Magick.Native/Stdafx.h
@@ -4,6 +4,12 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#if defined(MAGICK_NATIVE_WINDOWS)
+#pragma warning(disable : 4061) // switch of enum is not explicitly handled by a case label
+#pragma warning(disable : 4820) // bytes padding added after data member 
+#pragma warning(disable : 5045) // compiler will insert Spectre mitigation
+#endif
+
 #include <MagickCore/MagickCore.h>
 #include <MagickCore/studio.h>
 #include <MagickCore/utility-private.h>
@@ -24,9 +30,6 @@
 #else
 #define _LIB
 
-#pragma warning(disable : 4710)
-#pragma warning(disable : 4711)
-
 #define MAGICK_NATIVE_STRINGIFY(s) #s
 #if defined(_DEBUG)
 #define MAGICK_NATIVE_LINK_LIB(name) \
@@ -38,109 +41,109 @@
 
 #define MAGICK_NATIVE_EXPORT __declspec(dllexport)
 
-MAGICK_NATIVE_LINK_LIB("coders")
-MAGICK_NATIVE_LINK_LIB("MagickCore")
-MAGICK_NATIVE_LINK_LIB("MagickWand")
+MAGICK_NATIVE_LINK_LIB(coders)
+MAGICK_NATIVE_LINK_LIB(MagickCore)
+MAGICK_NATIVE_LINK_LIB(MagickWand)
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "urlmon.lib")
 
 #if defined(MAGICKCORE_BZLIB_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("bzlib")
+MAGICK_NATIVE_LINK_LIB(bzlib)
 #endif
 
 #if defined(MAGICKCORE_CAIRO_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("cairo")
+MAGICK_NATIVE_LINK_LIB(cairo)
 #endif
 
-#if defined(MAGICKCORE_FREETYPE_DELEGATE)g
-MAGICK_NATIVE_LINK_LIB("freetype")
+#if defined(MAGICKCORE_FREETYPE_DELEGATE)
+MAGICK_NATIVE_LINK_LIB(freetype)
 #endif
 
 #if defined(MAGICKCORE_HEIC_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("aom")
-MAGICK_NATIVE_LINK_LIB("de265")
-MAGICK_NATIVE_LINK_LIB("heif")
+MAGICK_NATIVE_LINK_LIB(aom)
+MAGICK_NATIVE_LINK_LIB(de265)
+MAGICK_NATIVE_LINK_LIB(heif)
 #endif
 
 #if defined(MAGICKCORE_JP2_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("jasper")
+MAGICK_NATIVE_LINK_LIB(jasper)
 #endif
 
 #if defined(MAGICKCORE_JPEG_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("jpeg-turbo")
+MAGICK_NATIVE_LINK_LIB(jpeg-turbo)
 #endif
 
 #if defined(MAGICKCORE_JXL_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("highway")
-MAGICK_NATIVE_LINK_LIB("brotli")
-MAGICK_NATIVE_LINK_LIB("jpeg-xl")
+MAGICK_NATIVE_LINK_LIB(highway)
+MAGICK_NATIVE_LINK_LIB(brotli)
+MAGICK_NATIVE_LINK_LIB(jpeg-xl)
 #endif
 
 #if defined(MAGICKCORE_LCMS_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("lcms")
+MAGICK_NATIVE_LINK_LIB(lcms)
 #endif
 
 #if defined(MAGICKCORE_LIBOPENJP2_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("openjpeg")
+MAGICK_NATIVE_LINK_LIB(openjpeg)
 #endif
 
 #if defined(MAGICKCORE_LQR_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("ffi")
-MAGICK_NATIVE_LINK_LIB("glib")
-MAGICK_NATIVE_LINK_LIB("lqr")
+MAGICK_NATIVE_LINK_LIB(ffi)
+MAGICK_NATIVE_LINK_LIB(glib)
+MAGICK_NATIVE_LINK_LIB(lqr)
 #pragma comment(lib, "winmm.lib")
 #endif
 
 #if defined(MAGICKCORE_LZMA_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("lzma")
+MAGICK_NATIVE_LINK_LIB(lzma)
 #endif
 
 #if defined(MAGICKCORE_OPENEXR_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("exr")
+MAGICK_NATIVE_LINK_LIB(exr)
 #endif
 
 #if defined(MAGICKCORE_PANGOCAIRO_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("fribidi")
-MAGICK_NATIVE_LINK_LIB("harfbuzz")
-MAGICK_NATIVE_LINK_LIB("pango")
-MAGICK_NATIVE_LINK_LIB("pixman")
+MAGICK_NATIVE_LINK_LIB(fribidi)
+MAGICK_NATIVE_LINK_LIB(harfbuzz)
+MAGICK_NATIVE_LINK_LIB(pango)
+MAGICK_NATIVE_LINK_LIB(pixman)
 #endif
 
 #if defined(MAGICKCORE_PNG_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("png")
+MAGICK_NATIVE_LINK_LIB(png)
 #endif
 
 #if defined(MAGICKCORE_RAQM_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("raqm")
+MAGICK_NATIVE_LINK_LIB(raqm)
 #endif
 
 #if defined(MAGICKCORE_RAW_R_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("raw")
+MAGICK_NATIVE_LINK_LIB(raw)
 #endif
 
 #if defined(MAGICKCORE_RSVG_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("croco")
-MAGICK_NATIVE_LINK_LIB("rsvg")
+MAGICK_NATIVE_LINK_LIB(croco)
+MAGICK_NATIVE_LINK_LIB(rsvg)
 #endif
 
 #if defined(MAGICKCORE_TIFF_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("tiff")
+MAGICK_NATIVE_LINK_LIB(tiff)
 #endif
 
 #if defined(MAGICKCORE_WEBP_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("webp")
+MAGICK_NATIVE_LINK_LIB(webp)
 #endif
 
 #if defined(MAGICKCORE_XML_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("xml")
+MAGICK_NATIVE_LINK_LIB(xml)
 #endif
 
 #if defined(MAGICKCORE_ZIP_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("zip")
+MAGICK_NATIVE_LINK_LIB(zip)
 #endif
 
 #if defined(MAGICKCORE_ZLIB_DELEGATE)
-MAGICK_NATIVE_LINK_LIB("zlib")
+MAGICK_NATIVE_LINK_LIB(zlib)
 #endif
 
 #endif
