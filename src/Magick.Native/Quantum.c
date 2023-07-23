d227ae89100c54d44ecf1d56bcffdbe0f32a465e
Dirk Lemstra <dirk@lemstra.org>
2023-07-23 07:58:04 +0200

    Return QuantumRange instead.

--- src/Magick.Native/Quantum.c
+++ src/Magick.Native/Quantum.c
@@ -11,13 +11,7 @@ MAGICK_NATIVE_EXPORT size_t Quantum_Depth_Get(void)
 
 MAGICK_NATIVE_EXPORT Quantum Quantum_Max_Get(void)
 {
-#if (MAGICKCORE_QUANTUM_DEPTH == 8)
-  return (Quantum) 255;
-#elif (MAGICKCORE_QUANTUM_DEPTH == 16)
-  return (Quantum) 65535;
-#else
-#error Not implemented!
-#endif
+  return QuantumRange;
 }
 
 MAGICK_NATIVE_EXPORT unsigned char Quantum_ScaleToByte(const Quantum value)
