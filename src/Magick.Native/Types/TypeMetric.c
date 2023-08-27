9ef6fbdf33fc6268a6a2e8e6fe1a71d9349f6632
Dirk Lemstra <dirk@lemstra.org>
2023-08-27 09:06:30 +0200

    Removed const from return type.

--- src/Magick.Native/Types/TypeMetric.c
+++ src/Magick.Native/Types/TypeMetric.c
@@ -14,37 +14,37 @@ MAGICK_NATIVE_EXPORT void TypeMetric_Dispose(TypeMetric *instance)
   RelinquishMagickMemory(instance);
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_Ascent_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_Ascent_Get(const TypeMetric *instance)
 {
   return instance->ascent;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_Descent_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_Descent_Get(const TypeMetric *instance)
 {
   return instance->descent;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_MaxHorizontalAdvance_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_MaxHorizontalAdvance_Get(const TypeMetric *instance)
 {
   return instance->max_advance;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_TextHeight_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_TextHeight_Get(const TypeMetric *instance)
 {
   return instance->height;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_TextWidth_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_TextWidth_Get(const TypeMetric *instance)
 {
   return instance->width;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_UnderlinePosition_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_UnderlinePosition_Get(const TypeMetric *instance)
 {
   return instance->underline_position;
 }
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_UnderlineThickness_Get(const TypeMetric *instance)
+MAGICK_NATIVE_EXPORT double TypeMetric_UnderlineThickness_Get(const TypeMetric *instance)
 {
   return instance->underline_thickness;
 }
--- src/Magick.Native/Types/TypeMetric.h
+++ src/Magick.Native/Types/TypeMetric.h
@@ -6,16 +6,16 @@ MAGICK_NATIVE_EXPORT TypeMetric *TypeMetric_Create(void);
 
 MAGICK_NATIVE_EXPORT void TypeMetric_Dispose(TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_Ascent_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_Ascent_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_Descent_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_Descent_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_MaxHorizontalAdvance_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_MaxHorizontalAdvance_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_TextHeight_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_TextHeight_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_TextWidth_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_TextWidth_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_UnderlinePosition_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_UnderlinePosition_Get(const TypeMetric *);
 
-MAGICK_NATIVE_EXPORT const double TypeMetric_UnderlineThickness_Get(const TypeMetric *);
+MAGICK_NATIVE_EXPORT double TypeMetric_UnderlineThickness_Get(const TypeMetric *);
