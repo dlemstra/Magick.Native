093e24ae625c7226e8d01f0910261ec16ec6df08
Dirk Lemstra <dirk@lemstra.org>
2023-08-31 23:45:02 +0200

    Also use symver for the Linux arm64 build to prevent linking with a recent version of GLIBC.

--- src/Magick.Native/Stdafx.c
+++ src/Magick.Native/Stdafx.c
@@ -3,26 +3,31 @@
 
 #include "Stdafx.h"
 
-#ifdef MAGICK_NATIVE_LINUX
-asm(".symver old_powf, powf@GLIBC_2.2.5");
+#if defined(MAGICK_NATIVE_LINUX)
+#define GLIBC_SYMVER_VERSION "2.2.5"
+#elif defined(MAGICK_NATIVE_LINUX_ARM64)
+#define GLIBC_SYMVER_VERSION "2.17"
+#endif
+
+#if defined GLIBC_SYMVER_VERSION
+asm(".symver old_powf, powf@GLIBC_"GLIBC_SYMVER_VERSION);
 float old_powf(float base, float exponent);
 float powf(float base, float exponent)
 {
   return old_powf(base, exponent);
 }
 
-asm(".symver old_logf, logf@GLIBC_2.2.5");
+asm(".symver old_logf, logf@GLIBC_"GLIBC_SYMVER_VERSION);
 float old_logf(float arg);
 float logf(float arg)
 {
   return old_logf(arg);
 }
 
-asm(".symver old_log2f, log2f@GLIBC_2.2.5");
+asm(".symver old_log2f, log2f@GLIBC_"GLIBC_SYMVER_VERSION);
 float old_log2f(float arg);
 float log2f(float arg)
 {
   return old_log2f(arg);
 }
-
 #endif
