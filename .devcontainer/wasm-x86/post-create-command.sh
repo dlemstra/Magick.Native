0737d6056853495b88efd7da0a442308a8e9b553
Dirk Lemstra <dirk@lemstra.org>
2023-09-09 14:56:47 +0200

    Use the new Dockerfile for the wasm build.

--- .devcontainer/wasm-x86/post-create-command.sh
+++ .devcontainer/wasm-x86/post-create-command.sh
@@ -6,3 +6,8 @@ ln -s /ImageMagick/libraries libraries
 
 cd src/Magick.Native
 ../../build/shared/build.Native.sh wasm x86
+
+# we don't want to build this in the devcontainer
+touch Q8/magick.d.ts
+
+../../build/wasm-x86/copy.Native.sh
