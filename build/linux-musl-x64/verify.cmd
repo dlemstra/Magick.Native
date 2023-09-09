20c6b6d7681e0d72d4cc76a841c242b00e950960
Dirk Lemstra <dirk@lemstra.org>
2023-09-09 19:38:09 +0200

    Updated verify.cmd

--- build/linux-musl-x64/verify.cmd
+++ build/linux-musl-x64/verify.cmd
@@ -1,5 +1,12 @@
 @echo off
 
-set image=alpine:latest
+set config=linux-musl
+set arch=x64
+rem set openmp=OpenMP
 
-docker run -v %~dp0../:/build %image% /bin/sh -c "apk update && apk add bash && /build/shared/verify.Native.sh linux-musl x64 /build/linux-musl-x64/output"
+set image=alpine:latest
+rem set image=mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+rem set image=mcr.microsoft.com/dotnet/runtime:3.1-alpine
+rem set image=mcr.microsoft.com/dotnet/runtime:5.0-alpine
+
+docker run -v %~dp0../:/build %image% /bin/sh -c "apk update ; apk add bash ; /build/shared/verify.Native.sh /build/%config%-%arch%/output %config% %arch% %openmp%"
