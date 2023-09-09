8a2f4f6288053ef8428f817970a57f1f56ec2b8c
Dirk Lemstra <dirk@lemstra.org>
2023-09-09 12:43:37 +0200

    The verify.cmd file now uses the output of the devcontainer.

--- build/linux-x64/verify.cmd
+++ build/linux-x64/verify.cmd
@@ -1,5 +1,18 @@
 @echo off
 
 set image=ubuntu:16.04
+rem set image=ubuntu:16.04
+rem set image=ubuntu:18.04
+rem set image=ubuntu:20.04
+rem set image=ubuntu:latest
+rem set image=tgagor/centos:stream8
+rem set image=tgagor/centos:stream9
+rem set image=mcr.microsoft.com/dotnet/aspnet:latest
+rem set image=mcr.microsoft.com/dotnet/runtime:3.1
+rem set image=mcr.microsoft.com/dotnet/runtime:latest
+rem set image=amazon/aws-lambda-dotnet:6
+rem set image=public.ecr.aws/lambda/dotnet:6-x86_64
 
-docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh linux x64 /build/linux-x64/output
+rem set openmp=OpenMP
+
+docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh /build/output linux x64 %openmp%
