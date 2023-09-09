7ff47f62f260e54bc737699adf91243da6d2eddf
Dirk Lemstra <dirk@lemstra.org>
2023-09-09 13:51:27 +0200

    Moved output folder back to the linux-x64 folder.

--- build/linux-x64/verify.cmd
+++ build/linux-x64/verify.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+set config=linux
+set arch=x64
+rem set openmp=OpenMP
+
 set image=ubuntu:16.04
 rem set image=ubuntu:16.04
 rem set image=ubuntu:18.04
@@ -13,6 +17,4 @@ rem set image=mcr.microsoft.com/dotnet/runtime:latest
 rem set image=amazon/aws-lambda-dotnet:6
 rem set image=public.ecr.aws/lambda/dotnet:6-x86_64
 
-rem set openmp=OpenMP
-
-docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh /build/output linux x64 %openmp%
+docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh /build/%config%-%arch%/output %config% %arch% %openmp%
