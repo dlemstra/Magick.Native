5b82ad5405b8321564e5a8f5244fe3751c2cdb01
Dirk Lemstra <dirk@lemstra.org>
2023-09-09 16:26:06 +0200

    Updated verify.cmd

--- build/linux-arm64/verify.cmd
+++ build/linux-arm64/verify.cmd
@@ -1,6 +1,11 @@
 @echo off
 
-set image=arm64v8/ubuntu
+set config=linux
+set arch=arm64
+rem set openmp=OpenMP
+
+set image=mcr.microsoft.com/dotnet/runtime:6.0-focal-arm64v8
+rem set image=public.ecr.aws/lambda/dotnet:6-arm64
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh linux arm64 /build/linux-arm64/output
+docker run -v %~dp0../:/build %image% /build/shared/verify.Native.sh /build/%config%-%arch%/output %config% %arch% %openmp%
