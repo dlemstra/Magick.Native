f3c36e1f9be93c22b0e85a7c4e48514722552abe
Dirk Lemstra <dirk@lemstra.org>
2023-09-25 20:01:36 +0200

    Switch to ubuntu:20.04 with the linux arm64 build.

--- build/linux-arm64/install.dependencies.sh
+++ build/linux-arm64/install.dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update -y
 
-apt-get install -y autoconf autopoint binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gettext git gperf libtool nasm pkg-config python python-pip python3-pip ragel texinfo
+apt-get install -y autoconf autopoint binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gettext git gperf libtool nasm pkg-config python python3-pip ragel texinfo
 
 pip3 install --upgrade pip
 pip3 install cmake==3.24.1.1
