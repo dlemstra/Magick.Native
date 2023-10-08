311f1fb0e2e0b07cf48dac97628bebc42155969a
Dirk Lemstra <dirk@lemstra.org>
2023-10-08 13:15:38 +0200

    Fixed typo.

--- build/libraries/build.cairo.sh
+++ build/libraries/build.cairo.sh
@@ -8,7 +8,7 @@ rm src/config.h
 
 mkdir __build
 cd __build
-CFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dxlib=disabled -Dquartz=disabled -Dfontconfig=enabled -ffreetype=enabled -Dtests=disabled
+CFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dxlib=disabled -Dquartz=disabled -Dfontconfig=enabled -Dfreetype=enabled -Dtests=disabled
 
 sed -i 's/#define CAIRO_HAS_PTHREAD 1/#define CAIRO_NO_MUTEX 1/' config.h
 
