caf3e04036d8630ca10eacb97a9d41af8ccf60bb
Dirk Lemstra <dirk@lemstra.org>
2023-11-08 07:08:01 +0100

    The output file is still called magick.js

--- build/wasm-x86/copy.Native.sh
+++ build/wasm-x86/copy.Native.sh
@@ -3,6 +3,6 @@ set -e
 
 [ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
 
-cp Q8/magick.mjs $1/magick.mjs
+cp Q8/magick.js $1/magick.js
 cp Q8/magick.wasm $1/magick.wasm
 cp Q8/magick.d.ts $1/magick.d.ts
