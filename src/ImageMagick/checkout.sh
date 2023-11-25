fe0cc1ee010d4553e8eafed91147123d0d5a9acb
Dirk Lemstra <dirk@lemstra.org>
2023-11-25 15:27:47 +0100

    Use commit of croco instead.

--- src/ImageMagick/checkout.sh
+++ src/ImageMagick/checkout.sh
@@ -187,7 +187,7 @@ if [ "$1" == "wasm" ]; then
 fi
 
 clone_date 'bzlib' "$commitDate"
-clone_date 'cairo' "$commitDate"
+clone_commit 'cairo' "dfa57b1ec4da76d56d37a99105f801f7cae6622c"
 clone_date 'croco' "$commitDate"
 clone_date 'fribidi' "$commitDate"
 clone_date 'harfbuzz' "$commitDate"
