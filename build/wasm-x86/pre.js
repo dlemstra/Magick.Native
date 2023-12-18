2f127b7ab9162aee0439bde01c1a8c6da455767b
Pepijn Peeters <me@pepijnpeeters.nl>
2023-12-18 12:53:09 +0100

    Use globalThis in pre.js
    Use globalThis in pre.js

--- build/wasm-x86/pre.js
+++ build/wasm-x86/pre.js
@@ -1,6 +1,3 @@
-(function checkCryptoPolyfill() {
-    let globalObject = typeof window != 'undefined' ? window : global;
-    if (typeof globalObject.crypto == 'undefined' || typeof crypto['getRandomValues'] != 'function') {
-        globalObject.crypto = { getRandomValues: (array) => { for (let i = 0; i < array.length; i++) array[i] = (Math.random() * 256) | 0 } };
-    }
-})();
+if (!globalThis.crypto?.getRandomValues) {
+    globalThis.crypto = { getRandomValues: (array) => { for (let i = 0; i < array.length; i++) array[i] = (Math.random() * 256) | 0 } };
+}
