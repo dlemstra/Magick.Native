43d90b284c2f1b9f93992d5d72d452508def2c99
Dirk Lemstra <dirk@lemstra.org>
2024-01-14 14:57:51 +0100

    Also copy the new constants file to the artifacts.

--- build/wasm-x86/copy.Native.sh
+++ build/wasm-x86/copy.Native.sh
@@ -4,5 +4,6 @@ set -e
 [ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
 
 cp Q8/magick.js $1/magick.js
-cp Q8/magick.wasm $1/magick.wasm
+cp Q8/magick.constants.ts $1/magick.constants.ts
 cp Q8/magick.d.ts $1/magick.d.ts
+cp Q8/magick.wasm $1/magick.wasm
