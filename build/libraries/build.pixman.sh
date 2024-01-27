ca778f3d6e20e6fcc3aa1aa074bc240f87548578
Dirk Lemstra <dirk@lemstra.org>
2024-01-27 14:51:36 +0100

    Use meson build for pixman instead.

--- build/libraries/build.pixman.sh
+++ build/libraries/build.pixman.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd pixman
-autoreconf -fiv
-chmod +x ./configure
-$CONFIGURE $CONFIGURE_OPTIONS --disable-shared --disable-arm-neon --disable-arm-a64-neon --prefix=/usr/local CFLAGS="$FLAGS"
-$MAKE install
+mkdir __build
+cd __build
+CFLAGS=$FLAGS meson .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=/usr/local --default-library=static -Dtests=disabled -Ddemos=disabled
+ninja install
