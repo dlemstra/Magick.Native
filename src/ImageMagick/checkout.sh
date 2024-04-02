e4ace9a67ea166b0a1b9f36b6b35ef3690741dc0
Dirk Lemstra <dirk@lemstra.org>
2024-04-02 17:37:45 +0200

    Changed checkout.sh to use the ImageMagick-Windows repository.

--- src/ImageMagick/checkout.sh
+++ src/ImageMagick/checkout.sh
@@ -1,227 +1,175 @@
 #!/bin/bash
 set -e
 
-clone() {
-    local repo=$1
-    local root=$2
+function cloneImageMagickWindows()
+{
+    local platform=$1
 
-    echo ''
-    echo "Cloning $repo"
-
-    if [ -z "$root" ]; then
-        root="https://github.com/ImageMagick"
+    if [ ! -d "imagemagick-windows" ]; then
+        git clone https://github.com/ImageMagick/ImageMagick-Windows imagemagick-windows
+    else
+        cd imagemagick-windows
+        git reset --hard
+        git pull origin main
+        cd ..
     fi
 
-    if [ ! -d "$repo" ]; then
-        git clone $root/$repo.git $repo
-        if [ $? != 0 ]; then echo "Error during checkout"; exit; fi
+    if [ "$platform" == "wasm" ]; then
+         rm -f imagemagick-windows/Dependencies/bzlib/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/cairo/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/croco/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/fribidi/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/harfbuzz/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/lzma/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/pango/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/pixman/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/rsvg/.ImageMagick/Config.txt
+         rm -f imagemagick-windows/Dependencies/zip/.ImageMagick/Config.txt
     fi
-    cd $repo
-    git reset --hard
-    git pull origin main
-    cd ..
+
+    if [ "$platform" != "windows" ]; then
+         rm -f imagemagick-windows/Dependencies/jasper/.ImageMagick/Config.txt
+    fi
+
+    rm -f imagemagick-windows/Projects/demos/.ImageMagick/Config.txt
+    rm -f imagemagick-windows/Projects/filters/.ImageMagick/Config.txt
+    rm -f imagemagick-windows/Projects/Magick++/.ImageMagick/Config.txt
+    rm -f imagemagick-windows/Projects/oss-fuzz/.ImageMagick/Config.txt
+    rm -f imagemagick-windows/Projects/utilities/.ImageMagick/Config.txt
 }
 
-clone_commit()
+function cloneRepositories()
 {
-    local repo=$1
-    local commit=$2
-    local root=$3
+    commit=$(<ImageMagick.commit)
 
-    clone $repo $root
-
-    cd $repo
-    git checkout $commit
+    cd imagemagick-windows
+    bash CloneRepositories.sh ImageMagick $commit
     cd ..
 }
 
-clone_date()
+function cloneFontconfig()
 {
-    local repo=$1
-    local date=$2
-    local root=$3
+    local platform=$1
 
-    clone $repo $root
-
-    cd $repo
-    git checkout `git rev-list -n 1 --before="$date" origin/main`
-    cd ..
+    if [ "$platform" == "macos" ] || [ "$platform" == "linux" ]; then
+        cd imagemagick-windows/Dependencies
+        if [ ! -d "fontconfig" ]; then
+            git clone https://github.com/dlemstra/fontconfig fontconfig
+        else
+            cd fontconfig
+            git reset --hard
+            git pull origin main
+            git checkout ef3d58fd09cd1c112fe82e53e9106b5dd52c37a1
+            cd ..
+        fi
+        cd ../../
+    fi
 }
 
-get_charset()
+function getCharset()
 {
     local file=$1
 
-    local charset="$(file -bi $file | awk -F "=" '{print $2}')"
+    charset="$(file -bi $file | awk -F "=" '{print $2}')"
     if [ -z "$charset" ]; then
         charset="$(file -I $file | awk -F "=" '{print $2}')"
     fi
     echo $charset
 }
 
-add_copyright()
+function addCopyright()
 {
     local copyrightFile=$1
     local notice=$2
 
-    local charset="$(get_charset $copyrightFile)"
+    charset="$(getCharset $copyrightFile)"
 
     echo -e "Adding notice from '$copyrightFile' ($charset)"
     iconv -f $charset -t utf-8 $copyrightFile | sed -e 's/\xef\xbb\xbf//' | tr -d '\r' >> $notice
     echo -e "" >> $notice
 }
 
-get_imagemagick_version()
+function getImagemagickVersion()
 {
-    local charset="$(get_charset "ImageMagick/m4/version.m4")"
-    local version="$(iconv -f $charset -t utf-8 ImageMagick/m4/version.m4 | sed -e 's/\xef\xbb\xbf//' | sed -e '/[rol]_version/! s/.*//' | sed -e 's/m4//' | sed -e 's/patchlevel_version/-/' | sed -e 's/minor_version/./' | sed -e 's/micro_version/./' | sed -e 's/[^0-9\.-]*//g' | tr -d '\r' | tr -d '\n')"
+    local folder=$1
 
-    local tag="$(cd ImageMagick && git describe --exact-match --tags HEAD && cd ..)"
+    charset="$(getCharset "$folder/m4/version.m4")"
+    version="$(iconv -f $charset -t utf-8 $folder/m4/version.m4 | sed -e 's/\xef\xbb\xbf//' | sed -e '/[rol]_version/! s/.*//' | sed -e 's/m4//' | sed -e 's/patchlevel_version/-/' | sed -e 's/minor_version/./' | sed -e 's/micro_version/./' | sed -e 's/[^0-9\.-]*//g' | tr -d '\r' | tr -d '\n')"
+
+    tag=`git -C $folder describe --exact-match --tags HEAD 2>/dev/null`
     if [ -z "$tag" ]; then
         version="$version beta"
     fi
+
+    date=`git -C $folder log -1 --format=%ci`
+    date="$(echo $date | sed 's/ .*//')"
+    version="$version ($date)"
+
     echo $version
 }
 
-create_notice()
+function createNotice()
 {
-    local date=$1
-    local output=$2
+    local output=$1
 
     if [ -z "$output" ]; then
-        output=../output
+        output="output"
     fi
 
     mkdir -p $output
-    local notice=$output/NOTICE
+    notice=$output/NOTICE
 
     echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" > $notice
     echo -e "[ Magick.Native ] copyright:\n" >> $notice
-    add_copyright '../../Magick.Native/Copyright.txt' $notice
+    addCopyright "../Magick.Native/Copyright.txt" $notice
 
     echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
-    local version=$(get_imagemagick_version)
-    date="$(echo $date | sed 's/ .*//')"
-    echo -e "[ ImageMagick $version ($date) ] copyright:\n" >> $notice
-    add_copyright 'ImageMagick/LICENSE' $notice
+    folder="imagemagick-windows/ImageMagick"
+    version=$(getImagemagickVersion $folder)
+    echo -e "[ ImageMagick $version ] copyright:\n" >> $notice
+    addCopyright "$folder/LICENSE" $notice
 
-    for dir in *; do
-        local config=VisualMagick/$dir/Config.txt
-        if [ -f "$config" ]; then
-            local copyright="$(sed -n '/\[LICENSE\]/{n;p;}' $config | tr -d '[:space:]' | sed -e 's/\.\.\\//g' | sed -e 's/\\/\//g')"
-            local fileNames=$(echo $copyright | tr ";" "\n")
-
-            echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
-
-            for fileName in $fileNames
-            do
-                fileName="$dir/$fileName"
-                if [ ! -f "$fileName" ]; then
-                    echo -e "Unable to find '$fileName'"
-                else
-                    local folder=$(dirname $fileName)
-                    local versionFile="$folder/ImageMagick/ImageMagick.version.h"
-                    if [ ! -f "$versionFile" ]; then
-                        folder=$(dirname $folder)
-                        versionFile="$folder/ImageMagick/ImageMagick.version.h"
-                        if [ ! -f "$versionFile" ]; then
-                            echo -e "Unable to find '$versionFile'"
-                            continue
-                        fi
-                    fi
-
-                    version="$(tail -n +2 $versionFile | awk -F " " '{print $3 " " $4}' | sed -e 's/"//g')"
-                    echo -e "[ $(basename $folder) $version ] copyright:\n" >> $notice
-
-                    add_copyright $fileName $notice
-                fi
-            done
+    for dir in imagemagick-windows/Dependencies/* ; do
+        config=$dir/.ImageMagick/Config.txt
+        if [ ! -f "$config" ]; then
+            echo -e "Unable to find '$config'"
+            continue
         fi
-    done
+        copyright="$(sed -n '/\[LICENSE\]/{n;p;}' $config | tr -d '[:space:]' | sed -e 's/\.\.\\//g' | sed -e 's/\\/\//g')"
+        fileNames=$(echo $copyright | tr ";" "\n")
 
-    echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
+        echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
+
+        for fileName in $fileNames
+        do
+            fileName="$dir/$fileName"
+            if [ ! -f "$fileName" ]; then
+                echo -e "Unable to find '$fileName'"
+                continue
+            fi
+            versionFolder=$(dirname $fileName)
+            versionFile="$versionFolder/.ImageMagick/ImageMagick.version.h"
+            if [ ! -f "$versionFile" ]; then
+                versionFolder=$(dirname $versionFolder)
+                versionFile="$versionFolder/.ImageMagick/ImageMagick.version.h"
+                if [ ! -f "$versionFile" ]; then
+                    echo -e "Unable to find '$versionFile'"
+                    continue
+                fi
+            fi
+
+            version="$(tail -n +2 $versionFile | awk -F " " '{print $3 " " $4}' | sed -e 's/"//g')"
+            echo -e "[ $(basename $folder) $version ] copyright:\n" >> $notice
+
+            addCopyright $fileName $notice
+        done
+    done
 }
 
-commit=$(<ImageMagick.commit)
+platform=$1
+noticeFolder=$2
 
-if [ ! -d "libraries" ]; then
-    mkdir libraries
-fi
-
-cd libraries
-
-clone_commit 'ImageMagick' $commit
-
-# get a commit date from the current ImageMagick checkout
-cd ImageMagick
-declare -r commitDate=`git log -1 --format=%ci`
-echo "Set latest commit date as $commitDate"
-cd ..
-
-clone_date 'aom' "$commitDate"
-clone_date 'brotli' "$commitDate"
-clone_date 'de265' "$commitDate"
-clone_date 'deflate' "$commitDate"
-clone_date 'exr' "$commitDate"
-clone_date 'freetype' "$commitDate"
-clone_date 'ffi' "$commitDate"
-clone_date 'glib' "$commitDate"
-clone_date 'heif' "$commitDate"
-clone_date 'highway' "$commitDate"
-clone_date 'jpeg-turbo' "$commitDate"
-clone_date 'jpeg-xl' "$commitDate"
-clone_date 'lcms' "$commitDate"
-clone_date 'lqr' "$commitDate"
-clone_date 'openjpeg' "$commitDate"
-clone_date 'png' "$commitDate"
-clone_date 'raw' "$commitDate"
-clone_date 'tiff' "$commitDate"
-clone_date 'VisualMagick' "$commitDate"
-clone_date 'webp' "$commitDate"
-clone_date 'xml' "$commitDate"
-clone_date 'zlib' "$commitDate"
-
-if [ "$1" == "wasm" ]; then
-    create_notice "$commitDate" $2
-    exit
-fi
-
-clone_date 'bzlib' "$commitDate"
-clone_date 'cairo' "$commitDate"
-clone_date 'croco' "$commitDate"
-clone_date 'fribidi' "$commitDate"
-clone_date 'harfbuzz' "$commitDate"
-clone_date 'lzma' "$commitDate"
-clone_date 'pango' "$commitDate"
-clone_date 'raqm' "$commitDate"
-clone_date 'pixman' "$commitDate"
-clone_date 'rsvg' "$commitDate"
-clone_date 'zip' "$commitDate"
-
-if [ "$1" == "macos" ] || [ "$1" == "linux" ] || [ "$1" == "linux-musl" ]; then
-    clone_commit 'fontconfig' "ca7dc14b14016d103ce595152b7c009c3d9c7d06" "https://github.com/dlemstra"
-
-    mkdir -p VisualMagick/fontconfig
-    echo -e "[LICENSE]\nCOPYING" > VisualMagick/fontconfig/Config.txt
-
-    create_notice "$commitDate" $2
-    exit
-fi
-
-clone_date 'jasper' "$commitDate"
-
-create_notice "$commitDate" $2
-
-rm -rf VisualMagick/dcraw
-rm -rf VisualMagick/demos
-rm -rf VisualMagick/fftw
-rm -rf VisualMagick/flif
-rm -rf VisualMagick/fuzz
-rm -rf VisualMagick/ImageMagickObject
-rm -rf VisualMagick/IMDisplay
-rm -rf VisualMagick/iptcutil
-rm -rf VisualMagick/jbig
-rm -rf VisualMagick/Magick++
-rm -rf VisualMagick/NtMagick
-rm -rf VisualMagick/tests
-rm -rf VisualMagick/utilities
+cloneImageMagickWindows $platform
+cloneRepositories
+cloneFontconfig $platform
+createNotice $noticeFolder
