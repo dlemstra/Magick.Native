9690e68100ddf214047010a2c88d79eaf44ccea3
Dirk Lemstra <dirk@lemstra.org>
2024-05-16 19:22:31 +0200

    Changed install of build tools for MacOS due to changes on the runner.

--- build/macos-x64/install.build-tools.sh
+++ build/macos-x64/install.build-tools.sh
@@ -8,56 +8,23 @@ brew list --formula
 brew install automake gnu-sed nasm ossp-uuid ragel
 
 # Uninstall already installed development libraries
+
 brew uninstall imagemagick
 
 brew uninstall composer
 brew uninstall php
-brew uninstall gd
-brew uninstall ghostscript
 brew uninstall ant
 brew uninstall gradle
 brew uninstall maven
 brew uninstall kotlin
 brew uninstall selenium-server
 brew uninstall sbt
-brew uninstall openjdk
 brew uninstall r
 brew uninstall colima
-brew uninstall lima
-brew uninstall qemu
-
-brew uninstall harfbuzz
-brew uninstall cairo
-brew uninstall libraw
-brew uninstall jasper
-brew uninstall libheif
-brew uninstall libde265
-brew uninstall x265
-brew uninstall liblqr
-brew uninstall libavif
-brew uninstall aom
-brew uninstall jpeg-xl
-brew uninstall highway
-brew uninstall webp
-brew uninstall openjpeg
-brew uninstall little-cms2
-brew uninstall libtiff
-brew uninstall jpeg-turbo
-brew uninstall libzip
-brew uninstall pixman
 
 brew uninstall libxft
 brew uninstall sox
 brew uninstall libxext
-brew uninstall libxrender
-brew uninstall libx11
-brew uninstall libslirp
-brew uninstall shared-mime-info
-
-brew uninstall fontconfig
-brew uninstall freetype
-brew uninstall libpng
-brew uninstall glib
 
 brew uninstall --ignore-dependencies brotli
 brew uninstall --ignore-dependencies xz
