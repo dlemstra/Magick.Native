b77e220977eb18c4864fce5d5d480799cf26a146
Dirk Lemstra <dirk@lemstra.org>
2024-06-16 23:01:01 +0200

    Corrected data type for quality.

--- src/Magick.Native/Optimizers/JpegOptimizer.c
+++ src/Magick.Native/Optimizers/JpegOptimizer.c
@@ -781,7 +781,7 @@ static void JpegThrowException(ClientData *client_data, ExceptionInfo *exception
     (void) ThrowException(exception, CorruptImageError, message, (const char *) NULL);
 }
 
-static void JpegOptimizer_Compress(ClientData *client_data, const MagickBooleanType progressive, const MagickBooleanType lossless, const MagickBooleanType quality, ExceptionInfo *exception)
+static void JpegOptimizer_Compress(ClientData *client_data, const MagickBooleanType progressive, const MagickBooleanType lossless, const size_t quality, ExceptionInfo *exception)
 {
   struct jpeg_decompress_struct
     decompress_info;
@@ -820,7 +820,7 @@ static void JpegOptimizer_Compress(ClientData *client_data, const MagickBooleanT
   TerminateClientData(client_data);
 }
 
-MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressFile(const char *input, const char *output, const MagickBooleanType progressive, const MagickBooleanType lossless, const MagickBooleanType quality, ExceptionInfo **exception)
+MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressFile(const char *input, const char *output, const MagickBooleanType progressive, const MagickBooleanType lossless, const size_t quality, ExceptionInfo **exception)
 {
   ClientData
     client_data;
@@ -835,7 +835,7 @@ MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressFile(const char *input, const ch
   MAGICK_NATIVE_SET_EXCEPTION;
 }
 
-MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressStream(const CustomStreamHandler reader, const CustomStreamHandler writer, const MagickBooleanType progressive, const MagickBooleanType lossless, const MagickBooleanType quality, ExceptionInfo **exception)
+MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressStream(const CustomStreamHandler reader, const CustomStreamHandler writer, const MagickBooleanType progressive, const MagickBooleanType lossless, const size_t quality, ExceptionInfo **exception)
 {
   ClientData
     client_data;
--- src/Magick.Native/Optimizers/JpegOptimizer.h
+++ src/Magick.Native/Optimizers/JpegOptimizer.h
@@ -6,9 +6,9 @@
 extern "C" {
 #endif
 
-MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressFile(const char *, const char *, const MagickBooleanType, const MagickBooleanType, const MagickBooleanType, ExceptionInfo **exception);
+MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressFile(const char *, const char *, const MagickBooleanType, const MagickBooleanType, const size_t, ExceptionInfo **exception);
 
-MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressStream(const CustomStreamHandler, const CustomStreamHandler, const MagickBooleanType, const MagickBooleanType, const MagickBooleanType, ExceptionInfo **exception);
+MAGICK_NATIVE_EXPORT void JpegOptimizer_CompressStream(const CustomStreamHandler, const CustomStreamHandler, const MagickBooleanType, const MagickBooleanType, const size_t, ExceptionInfo **exception);
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
