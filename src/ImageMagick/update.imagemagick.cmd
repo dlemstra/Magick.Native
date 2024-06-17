b28e7e8ac584e81be33867a4632eff11c3993b0e
Dirk Lemstra <dirk@lemstra.org>
2024-06-17 22:07:10 +0200

    Only update ImageMagick.commit in the pipeline and use latest instead of a sha to make sure all the repositories are at the latest commit.

--- src/ImageMagick/update.imagemagick.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call set.bash.cmd
-
-if not ERRORLEVEL 1 (
-    %BASH% --login -i -c "./update.imagemagick.sh"
-)
-
--- src/ImageMagick/update.imagemagick.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-commit=$(git ls-remote https://github.com/ImageMagick/ImageMagick.git HEAD)
-commit=${commit:0:40}
-echo "Update ImageMagick commit to: $commit"
-echo -n $commit > ImageMagick.commit
