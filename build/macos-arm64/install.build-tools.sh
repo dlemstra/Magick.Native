68833e220dc7acf51db8a1639c3ca3b22266f56c
Dirk Lemstra <dirk@lemstra.org>
2024-09-20 12:17:20 +0200

    Added extra checks to make sure all packages that will result in conflicts are uninstalled.

--- build/macos-arm64/install.build-tools.sh
+++ build/macos-arm64/install.build-tools.sh
@@ -24,10 +24,50 @@ brew uninstall libxft
 brew uninstall sox
 brew uninstall libxext
 
+brew uninstall jpeg-xl
+brew uninstall libpng
+
 brew uninstall --ignore-dependencies brotli
 brew uninstall --ignore-dependencies xz
 brew uninstall --ignore-dependencies zlib
 brew uninstall --ignore-dependencies zstd
+
+packages=(
+    "aom"
+    "brotli"
+    "cairo"
+    "fontconfig"
+    "freetype"
+    "glib"
+    "harfbuzz"
+    "highway"
+    "imagemagick"
+    "jasper"
+    "jbig2dec"
+    "jpeg-turbo"
+    "libheif"
+    "liblqr"
+    "libpng"
+    "libraw"
+    "libtiff"
+    "libzip"
+    "little-cms2"
+    "openjpeg"
+    "pixman"
+    "webp"
+    "x265"
+    "xz"
+    "zlib"
+    "zstd"
+)
+
+# Check if all packages are uninstalled
+for package in "${packages[@]}"; do
+    if brew list --formula | grep -q "^${package}\$"; then
+        echo "${package} should not be installed"
+        exit 1
+    fi
+done
 
 # Install build tools
 brew install automake libtool gnu-sed nasm ossp-uuid ragel
