d086d80d28f56ec34213aa3d8b7738eac8421d8e
Dirk Lemstra <dirk@lemstra.org>
2024-09-26 06:25:23 +0200

    Make configure executable.

--- build/dependencies/build.tiff.sh
+++ build/dependencies/build.tiff.sh
@@ -7,6 +7,7 @@ rm libtiff/tif_config.h
 rm libtiff/tiffconf.h
 
 autoreconf -fiv
+chmod +x ./configure
 $CONFIGURE $CONFIGURE_OPTIONS --disable-shared CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
 echo '' > version
 $MAKE install
