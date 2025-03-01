d58adc4df5faae5d1b6501dce62e323f47d3a1b5
Dirk Lemstra <dirk@lemstra.org>
2025-03-01 22:49:11 +0100

    Hack to fix missing gtkdocize executable when building rsvg on linux-musl.

--- build/linux-musl-x64/install.build-tools.sh
+++ build/linux-musl-x64/install.build-tools.sh
@@ -10,3 +10,6 @@ pip3 install --upgrade pip
 pip3 install cmake==3.30.3
 pip3 install meson==1.5.1
 pip3 install ninja==1.11.1.1
+
+echo '#!/bin/bash' > /usr/local/bin/gtkdocize
+chmod 755 /usr/local/bin/gtkdocize
