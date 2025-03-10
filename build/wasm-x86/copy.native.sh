872e7de15ce1318a56ebcfb931ea3554a98a557f
Dirk Lemstra <dirk@lemstra.org>
2025-03-10 22:22:51 +0100

    Patch to work around the removal of the USE_ES6_IMPORT_META option in new emscripten release. Removing import.meta.url makes sure that the wasm file is not embedded in the js file.

--- build/wasm-x86/copy.native.sh
+++ build/wasm-x86/copy.native.sh
@@ -3,7 +3,7 @@ set -e
 
 [ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
 
-cp Q8/magick.js $1/magick.js
+sed 's/,import.meta.url//' Q8/magick.js > $1/magick.js
 cp Q8/magick.constants.ts $1/magick.constants.ts
 cp Q8/magick.d.ts $1/magick.d.ts
 cp Q8/magick.wasm $1/magick.wasm
