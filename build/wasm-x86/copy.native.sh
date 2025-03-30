fb9e01fa180e4d0d8929a2e93646e63ee5b5b9cf
Dirk Lemstra <dirk@lemstra.org>
2025-03-30 18:20:14 +0200

    Corrected the hack to work around the remove of the USE_ES6_IMPORT_META option.

--- build/wasm-x86/copy.native.sh
+++ build/wasm-x86/copy.native.sh
@@ -3,7 +3,7 @@ set -e
 
 [ "$1" != "" ] && [ ! -d "$1" ] && mkdir "$1"
 
-sed 's/,import.meta.url//' Q8/magick.js > $1/magick.js
+sed 's#magick.wasm",import.meta.url#data:text/plain;base64,"#' Q8/magick.js > $1/magick.js
 cp Q8/magick.constants.ts $1/magick.constants.ts
 cp Q8/magick.d.ts $1/magick.d.ts
 cp Q8/magick.wasm $1/magick.wasm
