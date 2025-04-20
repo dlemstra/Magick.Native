2db395d2e880aa1b6fbdd8510a967535092435e8
Dirk Lemstra <dirk@lemstra.org>
2025-04-20 12:31:24 +0200

    Build configure in the target platform on Windows.

--- build/windows/build.configure.cmd
+++ build/windows/build.configure.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "..\..\tools\windows\init.visualstudio.cmd"
 
-powershell .\build.configure.ps1
+powershell .\build.configure.ps1 -platformName %1
 if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/build.configure.ps1
+++ build/windows/build.configure.ps1
@@ -1,6 +1,10 @@
 # Copyright Dirk Lemstra https://github.com/dlemstra/Magick.NET.
 # Licensed under the Apache License, Version 2.0.
 
+param (
+    [string]$platformName = $env:PlatformName
+)
+
 . $PSScriptRoot\..\..\tools\windows\utils.ps1
 
-build "src\ImageMagick\imagemagick-windows\Configure\Configure.sln" "Configuration=Release,Platform=x64"
+build "src\ImageMagick\imagemagick-windows\Configure\Configure.sln" "Configuration=Release,Platform=$platformName"
