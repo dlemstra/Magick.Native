e1e3a16b1c52c84a4f27b31e8d5176dfe1fdbb15
Dirk Lemstra <dirk@lemstra.org>
2025-07-25 21:08:18 +0200

    Corrected checkout for release.

--- src/ImageMagick/checkout-release.cmd
+++ src/ImageMagick/checkout-release.cmd
@@ -2,6 +2,9 @@
 
 set "SCRIPT_DIR=%~dp0"
 
+set architecture=%1
+set openMP=%2
+
 call "%SCRIPT_DIR%..\..\.github\build\windows\find-bash.cmd"
 
-%BASH% --login -i -c "./checkout.sh"
+%BASH% --login -i -c "./checkout.sh --dependencies-artifact windows-%architecture%-static-%openMP%-linked-runtime.zip"
