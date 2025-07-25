a7c490e8b5539ebb7e3b90d8c043097b1921f38d
Dirk Lemstra <dirk@lemstra.org>
2025-07-25 20:41:11 +0200

    Updated script to clone the ImageMagick repositories.

---
+++ src/ImageMagick/checkout-development.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+set "SCRIPT_DIR=%~dp0"
+
+call "%SCRIPT_DIR%..\..\.github\build\windows\find-bash.cmd"
+
+%BASH% --login -i -c "./checkout.sh --development"
--- src/ImageMagick/checkout.sh
+++ src/ImageMagick/checkout.sh
@@ -1,173 +1,96 @@
 #!/bin/bash
 set -e
 
-function cloneImageMagickWindows()
+clone()
 {
-    if [ ! -d "imagemagick-windows" ]; then
-        git clone https://github.com/ImageMagick/ImageMagick-Windows imagemagick-windows
-    else
-        cd imagemagick-windows
-        git reset --hard
-        git pull origin main
-        cd ..
+  local repo=$1
+  local folder=$2
+
+  echo ''
+  echo "Cloning $repo"
+
+  if [ -z "$folder" ]; then
+    folder=$repo
+  fi
+
+  if [ -d "$folder" ]; then
+    cd $folder
+  else
+    git clone https://github.com/ImageMagick/$repo.git $folder
+    if [ $? != 0 ]; then
+      echo "Error during checkout";
+      exit;
     fi
 
-    rm -rf imagemagick-windows/Projects/demos/.ImageMagick
-    rm -rf imagemagick-windows/Projects/filters/.ImageMagick
-    rm -rf imagemagick-windows/Projects/Magick++/.ImageMagick
-    rm -rf imagemagick-windows/Projects/oss-fuzz/.ImageMagick
-    rm -rf imagemagick-windows/Projects/utilities/.ImageMagick
+    cd $folder
+    git remote add sshpush git@github.com:ImageMagick/$repo.git
+    git config remote.pushDefault sshpush
+  fi
+
+  git reset --hard
+  git pull origin main
+  cd ..
 }
 
-function cloneRepositories()
+clone_imagemagick()
 {
-    local platform=$1
+  commit=$(<ImageMagick.commit)
 
-    commit=$(<ImageMagick.commit)
+  echo "Cloning ImageMagick"
+  clone ImageMagick
+  cd ImageMagick
 
-    cd imagemagick-windows
-    bash CloneRepositories.sh ImageMagick $commit
-    cd ..
+  if [ "$commit" != "latest" ]; then
+    echo "Checking out commit $commit"
+    git checkout $commit
+  else
+    echo "Checking out latest commit"
+  fi
 
-    if [ "$platform" == "wasm" ]; then
-         rm -rf imagemagick-windows/Dependencies/bzlib/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/cairo/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/croco/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/fribidi/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/harfbuzz/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/lzma/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/openh264/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/pango/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/pixman/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/raqm/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/rsvg/.ImageMagick
-         rm -rf imagemagick-windows/Dependencies/zip/.ImageMagick
-    fi
+  cd ..
 }
 
-function cloneFontconfig()
+download_configure()
 {
-    local platform=$1
-
-    if [ "$platform" == "macos" ] || [ "$platform" == "linux" ]; then
-        cd imagemagick-windows/Dependencies
-        if [ ! -d "fontconfig" ]; then
-            git clone https://github.com/dlemstra/fontconfig fontconfig
-        else
-            cd fontconfig
-            git reset --hard
-            git pull origin main
-            git checkout da591e98e0de5709c977807759dd5e1a95852620
-            cd ..
-        fi
-        cd ../../
-    fi
+  ./ImageMagick/.github/build/windows/download-configure.sh
 }
 
-function getCharset()
+clone_dependencies()
 {
-    local file=$1
+  echo "Cloning Dependencies"
+  clone Dependencies
+  cd Dependencies
 
-    charset="$(file -bi $file | awk -F "=" '{print $2}')"
-    if [ -z "$charset" ]; then
-        charset="$(file -I $file | awk -F "=" '{print $2}')"
-    fi
-    echo $charset
+  ./clone-dependencies.sh
+
+  cd ..
 }
 
-function addCopyright()
+download_dependencies()
 {
-    local copyrightFile=$1
-    local notice=$2
-
-    charset="$(getCharset $copyrightFile)"
-
-    echo -e "Adding notice from '$copyrightFile' ($charset)"
-    iconv -f $charset -t utf-8 $copyrightFile | sed -e 's/\xef\xbb\xbf//' | tr -d '\r' >> $notice
-    echo -e "" >> $notice
+  ./ImageMagick/.github/build/windows/download-dependencies.sh
 }
 
-function getImagemagickVersion()
-{
-    local folder=$1
+development=false
 
-    charset="$(getCharset "$folder/m4/version.m4")"
-    version="$(iconv -f $charset -t utf-8 $folder/m4/version.m4 | sed -e 's/\xef\xbb\xbf//' | sed -e '/[rol]_version/! s/.*//' | sed -e 's/m4//' | sed -e 's/patchlevel_version/-/' | sed -e 's/minor_version/./' | sed -e 's/micro_version/./' | sed -e 's/[^0-9\.-]*//g' | tr -d '\r' | tr -d '\n')"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --development)
+      development=true
+      shift 1
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
-    tag=`git -C $folder describe --exact-match --tags HEAD 2>/dev/null`
-    if [ -z "$tag" ]; then
-        version="$version beta"
-    fi
+clone_imagemagick
+download_configure
 
-    date=`git -C $folder log -1 --format=%ci`
-    date="$(echo $date | sed 's/ .*//')"
-    version="$version ($date)"
-
-    echo $version
-}
-
-function createNotice()
-{
-    local noticeFolder=$1
-
-    if [ -z "$noticeFolder" ]; then
-        noticeFolder="artifacts"
-    fi
-
-    mkdir -p $noticeFolder
-    notice=$noticeFolder/NOTICE
-
-    echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" > $notice
-    echo -e "[ Magick.Native ] copyright:\n" >> $notice
-    addCopyright "../Magick.Native/Copyright.txt" $notice
-
-    echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
-    folder="imagemagick-windows/ImageMagick"
-    version=$(getImagemagickVersion $folder)
-    echo -e "[ ImageMagick $version ] copyright:\n" >> $notice
-    addCopyright "$folder/LICENSE" $notice
-
-    for dir in imagemagick-windows/Dependencies/* ; do
-        config=$dir/.ImageMagick/Config.txt
-        if [ ! -f "$config" ]; then
-            echo -e "Unable to find '$config'"
-            continue
-        fi
-        copyright="$(sed -n '/\[LICENSE\]/{n;p;}' $config | tr -d '[:space:]' | sed -e 's/\.\.\\//g' | sed -e 's/\\/\//g')"
-        fileNames=$(echo $copyright | tr ";" "\n")
-
-        echo -e "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n" >> $notice
-
-        for fileName in $fileNames
-        do
-            fileName="$dir/$fileName"
-            if [ ! -f "$fileName" ]; then
-                echo -e "Unable to find '$fileName'"
-                continue
-            fi
-            versionFolder=$(dirname $fileName)
-            versionFile="$versionFolder/.ImageMagick/ImageMagick.version.h"
-            if [ ! -f "$versionFile" ]; then
-                versionFolder=$(dirname $versionFolder)
-                versionFile="$versionFolder/.ImageMagick/ImageMagick.version.h"
-                if [ ! -f "$versionFile" ]; then
-                    echo -e "Unable to find '$versionFile'"
-                    continue
-                fi
-            fi
-
-            version="$(tail -n +2 $versionFile | awk -F " " '{print $3 " " $4}' | sed -e 's/"//g')"
-            echo -e "[ $(basename $versionFolder) $version ] copyright:\n" >> $notice
-
-            addCopyright $fileName $notice
-        done
-    done
-}
-
-platform=$1
-noticeFolder=$2
-
-cloneImageMagickWindows
-cloneRepositories $platform
-cloneFontconfig $platform
-createNotice $noticeFolder
+if [ "$development" == true ]; then
+  clone_dependencies
+else
+  download_dependencies
+fi
