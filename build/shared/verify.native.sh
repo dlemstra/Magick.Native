9935b2bb7e943064252dd114333f175af151fc7a
Dirk Lemstra <dirk@lemstra.org>
2025-07-30 17:22:16 +0200

    Correct indentation and print which file is being verified.

--- build/shared/verify.native.sh
+++ build/shared/verify.native.sh
@@ -11,89 +11,91 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 
 verifyNative() {
-    local name=$1
-    if [ "$openmp" == "OpenMP" ]; then
-        name=${name}-OpenMP
-    fi
+  local name=$1
+  if [ "$openmp" == "OpenMP" ]; then
+    name=${name}-OpenMP
+  fi
 
-    local file=${folder}/Release${name}/$arch/Magick.Native-${name}-$arch.dll.$EXTENSION
+  local file=${folder}/Release${name}/$arch/Magick.Native-${name}-$arch.dll.$EXTENSION
 
-    if [ ! -f $file ]; then
-        echo "Unable to find $file"
-        exit 1
-    fi
+  if [ ! -f $file ]; then
+    echo "Unable to find $file"
+    exit 1
+  fi
 
-    exit_code=0
-    output=$(ldd $file 2>&1) || exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        echo "Failed to execute ldd: $output"
-        exit 1
-    fi
+  echo "Verifying $file"
 
-    if [ "$openmp" == "OpenMP" ]; then
-        if [[ "$output" != *"libgomp.so"* ]]; then
-            echo "Libgomp should be linked in $file"
-        fi
-    fi
+  exit_code=0
+  output=$(ldd $file 2>&1) || exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "Failed to execute ldd: $output"
+    exit 1
+  fi
 
-    if echo "$output" | grep "not found"; then
-        exit 1
-    else
-        echo "Verified ldd status for $file"
+  if [ "$openmp" == "OpenMP" ]; then
+    if [[ "$output" != *"libgomp.so"* ]]; then
+      echo "Libgomp should be linked in $file"
     fi
+  fi
 
-    output=$(ld $file 2>&1) || exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        echo "Failed to execute ld: $output"
-        exit 1
-    fi
+  if echo "$output" | grep "not found"; then
+    exit 1
+  else
+    echo "Verified ldd status for $file"
+  fi
 
-    if echo "$output" | grep "undefined reference"; then
-        exit 1
-    else
-        echo "Verified ld status for $file"
-    fi
+  output=$(ld $file 2>&1) || exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "Failed to execute ld: $output"
+    exit 1
+  fi
+
+  if echo "$output" | grep "undefined reference"; then
+    exit 1
+  else
+    echo "Verified ld status for $file"
+  fi
 }
 
 if [ -f "/bin/yum" ]; then
-    if [ "$openmp" == "OpenMP" ]; then
-        yum install -y libgomp
-    fi
+  if [ "$openmp" == "OpenMP" ]; then
+    yum install -y libgomp
+  fi
 
-    if [ ! -f "/usr/bin/ld" ]; then
-        yum install -y binutils
-    fi
+  if [ ! -f "/usr/bin/ld" ]; then
+    yum install -y binutils
+  fi
 elif [ -f "/bin/dnf" ]; then
-    if [ "$openmp" == "OpenMP" ]; then
-        dnf install -y libgomp
-    fi
+  if [ "$openmp" == "OpenMP" ]; then
+    dnf install -y libgomp
+  fi
 
-    if [ ! -f "/usr/bin/ld" ]; then
-        dnf install -y binutils
-    fi
+  if [ ! -f "/usr/bin/ld" ]; then
+    dnf install -y binutils
+  fi
 elif [ -f "/sbin/apk" ]; then
-    apk update
-    if [ "$openmp" == "OpenMP" ]; then
-        apk add libgomp
-    fi
+  apk update
+  if [ "$openmp" == "OpenMP" ]; then
+    apk add libgomp
+  fi
 
-    if [ ! -f "/usr/bin/ld" ]; then
-        apk add binutils
-    fi
+  if [ ! -f "/usr/bin/ld" ]; then
+    apk add binutils
+  fi
 else
-    apt-get update -y
-    if [ "$openmp" == "OpenMP" ]; then
-        apt-get install -y libgomp1
-    fi
+  apt-get update -y
+  if [ "$openmp" == "OpenMP" ]; then
+    apt-get install -y libgomp1
+  fi
 
-    if [ ! -f "/usr/bin/ld" ]; then
-        apt-get install -y binutils
-    fi
+  if [ ! -f "/usr/bin/ld" ]; then
+    apt-get install -y binutils
+  fi
 fi
 
 ldd --version || true
 
 for quantum in ${QUANTUMS[@]}; do
-    verifyNative $quantum
+  verifyNative $quantum
 done
 
