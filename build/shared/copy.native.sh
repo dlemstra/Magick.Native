65553c1f03c4d4b5f348996dcc5f09db15b49118
Dirk Lemstra <dirk@lemstra.org>
2025-08-02 10:45:27 +0200

    Also add quantum parameter to the copy native script.

--- build/shared/copy.native.sh
+++ build/shared/copy.native.sh
@@ -4,25 +4,24 @@ set -e
 folder=$1
 config=$2
 architecture=$3
-openmp=$4
+quantum=$4
+openmp=$5
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 . $SCRIPT_PATH/../$config-$architecture/settings.sh
 
 copyNative() {
-    local quantum=$1
-    local name=$quantum
-    if [ "$openmp" == "OpenMP" ]; then
-        name=${name}-OpenMP
-    fi
+  local quantum=$1
+  local name=$quantum
+  if [ "$openmp" == "OpenMP" ]; then
+    name=${name}-OpenMP
+  fi
 
-    mkdir -p $folder/Release$name
-    mkdir -p $folder/Release$name/$architecture
-    cp $quantum/libMagick.Native-$name-$architecture.dll.$EXTENSION $folder/Release$name/$architecture/Magick.Native-$name-$architecture.dll.$EXTENSION
+  mkdir -p $folder/Release$name
+  mkdir -p $folder/Release$name/$architecture
+  cp $quantum/libMagick.Native-$name-$architecture.dll.$EXTENSION $folder/Release$name/$architecture/Magick.Native-$name-$architecture.dll.$EXTENSION
 }
 
 [ "$folder" != "" ] && [ ! -d "$folder" ] && mkdir "$folder"
 
-for quantum in ${QUANTUMS[@]}; do
-    copyNative $quantum
-done
+copyNative $quantum
