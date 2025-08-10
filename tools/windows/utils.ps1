58e655d952d8cfcdb3e5a230933d7ed7c9b7cd01
Dirk Lemstra <dirk@lemstra.org>
2025-08-10 08:23:17 +0200

    Removed unused build method.

--- tools/windows/utils.ps1
+++ tools/windows/utils.ps1
@@ -14,18 +14,3 @@ function fullPath($path)
 {
     return "$PSScriptRoot\..\..\$path"
 }
-
-function build($fileName, $properties)
-{
-    $path = fullPath $fileName
-    $directory = Split-Path -parent $path
-    $file = Split-Path -leaf $path
-
-    $location = $(Get-Location)
-    Set-Location $directory
-
-    msbuild $file /t:Rebuild /m ("/p:$($properties)")
-    checkExitCode "Failed to build: $($path)"
-
-    Set-Location $location
-}
