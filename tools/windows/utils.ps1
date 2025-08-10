99864a70d29632d4efc4b81a7d825d542b19c8bd
Dirk Lemstra <dirk@lemstra.org>
2025-08-10 08:26:12 +0200

    Removed unused checkExitCode method.

--- tools/windows/utils.ps1
+++ tools/windows/utils.ps1
@@ -1,15 +1,6 @@
 # Copyright Dirk Lemstra https://github.com/dlemstra/Magick.Native.
 # Licensed under the Apache License, Version 2.0.
 
-function checkExitCode($msg)
-{
-    if ($LastExitCode -ne 0)
-    {
-        Write-Error $msg
-        Exit 1
-    }
-}
-
 function fullPath($path)
 {
     return "$PSScriptRoot\..\..\$path"
