fafb213d760a21bee1fb8ea7a6929072dd76e8fd
Dirk Lemstra <dirk@lemstra.org>
2026-04-20 23:06:20 +0200

    Corrected pip3 install.

--- build/linux-musl-x64/install.build-tools.sh
+++ build/linux-musl-x64/install.build-tools.sh
@@ -6,8 +6,5 @@ apk update
 apk add autoconf automake bash curl file g++ gcc git libtool make pkgconf python3 py3-pip unzip
 apk del ninja
 
-pip3 install --upgrade pip
-pip3 install cmake==3.30.3
-
-echo '#!/bin/bash' > /usr/local/bin/gtkdocize
-chmod 755 /usr/local/bin/gtkdocize
+pip3 install --break-system-packages --upgrade pip
+pip3 install --break-system-packages cmake==3.30.3
