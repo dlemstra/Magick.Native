5270fca5a385a0aa719a60865a8f8a642f0358d2
Dirk Lemstra <dirk@lemstra.org>
2026-08-13 21:55:53 +0200

    Corrected the options.

--- build/wasm-x64/settings.sh
+++ build/wasm-x64/settings.sh
@@ -1,9 +1,9 @@
 # Compiler settings
 if [[ "${DEBUG_BUILD}" ]]; then
-    export FLAGS="-g3 -O0 -Wall -DDEBUG -w64"
+    export FLAGS="-g3 -O0 -Wall -DDEBUG -m64"
     export CMAKE_BUILD_TYPE="Debug"
 else
-    export FLAGS="-O3 -Wall -DNDEBUG -w64"
+    export FLAGS="-O3 -Wall -DNDEBUG -m64"
     export CMAKE_BUILD_TYPE="Release"
 fi
 export CFLAGS=$FLAGS
--- build/wasm-x86/settings.sh
+++ build/wasm-x86/settings.sh
@@ -1,9 +1,9 @@
 # Compiler settings
 if [[ "${DEBUG_BUILD}" ]]; then
-    export FLAGS="-g3 -O0 -Wall -DDEBUG -w32"
+    export FLAGS="-g3 -O0 -Wall -DDEBUG -m32"
     export CMAKE_BUILD_TYPE="Debug"
 else
-    export FLAGS="-O3 -Wall -DNDEBUG -w32"
+    export FLAGS="-O3 -Wall -DNDEBUG -m32"
     export CMAKE_BUILD_TYPE="Release"
 fi
 export CFLAGS=$FLAGS
