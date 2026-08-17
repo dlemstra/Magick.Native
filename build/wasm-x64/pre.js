88e82e017da15454db0fea88a17b901c5fb60c61
Dirk Lemstra <dirk@lemstra.org>
2026-08-17 22:37:53 +0200

    Corrected value for null pointer.

--- build/wasm-x64/pre.js
+++ build/wasm-x64/pre.js
@@ -2,5 +2,5 @@ if (!globalThis.crypto || !globalThis.crypto.getRandomValues) {
     globalThis.crypto = { getRandomValues: (array) => { for (let i = 0; i < array.length; i++) array[i] = (Math.random() * 256) | 0 } };
 }
 Module["_CastToSize"] = (value) => BigInt(value);
-Module["_NullPointer"] = 0;
+Module["_NullPointer"] = 0n;
 Module["_PointerSize"] = 8;
