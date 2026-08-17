cd9f0e45394155ba7a4af157db570983fa0f1763
Dirk Lemstra <dirk@lemstra.org>
2026-08-17 19:54:32 +0200

    Removed extern-post scripts

--- build/wasm-x64/extern-post.js
+++
@@ -1,4 +0,0 @@
-export function CastToSize(value) { return BigInt(value) }
-export const NativePointerSize = 8;
-export const NativePointerType = 'i64';
-export const NativeZero = 0n;
--- build/wasm-x86/extern-post.js
+++
@@ -1,4 +0,0 @@
-export function CastToSize(value) { return value }
-export const NativePointerSize = 4;
-export const NativePointerType = 'i32';
-export const NativeZero = 0;
