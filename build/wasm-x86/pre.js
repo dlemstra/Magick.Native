dbbfb4d9fe6b341f130473b063967068366ea286
Dirk Lemstra <dirk@lemstra.org>
2026-08-17 19:56:50 +0200

    Add an api to the Module itself.

--- build/wasm-x86/pre.js
+++ build/wasm-x86/pre.js
@@ -1,3 +1,6 @@
 if (!globalThis.crypto || !globalThis.crypto.getRandomValues) {
     globalThis.crypto = { getRandomValues: (array) => { for (let i = 0; i < array.length; i++) array[i] = (Math.random() * 256) | 0 } };
 }
+Module["_CastToSize"] = (value) => value;
+Module["_NullPointer"] = 0;
+Module["_PointerSize"] = 4;
